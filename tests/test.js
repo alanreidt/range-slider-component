3ec687b1e4953224aec048dbd1c8e8ca0210238d
Alan Reid <alanreidt@gmail.com>
2019-08-22 18:25:28 +0300

    changed variables names and structure, according to domain model

--- tests/test.js
+++ tests/test.js
@@ -1,29 +1,22 @@
 import {DomainModel} from "../src/domainModel";
 
 describe("DomainModel", function() {
-  // describe("accepts only positive numbers", function() {
-  // });
-
-  describe("creates instance with default values", function() {
-    let newDomainModel = new DomainModel();
-    let newDomainModelOptions = newDomainModel.options;
-    let defaultDomainModel = {
-      min: 0,
-      max: 100,
-      values: [0, 100],
+  describe("shall assign default values, if nothing is passed", function() {
+    let subjectDomainModel = new DomainModel().options;
+    let objectDomainModel = {
+      boundaries: [0, 100],
+      value: [0, 100],
       step: null,
       orientation: "horizontal",
       tooltips: false,
     };
 
-    for (let key in newDomainModelOptions) {
+    for (let key in subjectDomainModel) {
 
-      it(`${key} is equal to ${defaultDomainModel[key]}`, function() {
-        assert.deepEqual( newDomainModelOptions[key], defaultDomainModel[key] );
+      it(`${key} is equal to ${objectDomainModel[key]}`, function() {
+        assert.deepEqual( subjectDomainModel[key], objectDomainModel[key] );
       });
 
     }
-
   });
-
 });
