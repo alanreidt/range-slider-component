7d83b932b4d3db06fb4c5607d93fe90bb3f41945
Alan Reid <alanreidt@gmail.com>
2019-08-29 16:55:11 +0300

    corrected domainmodel

--- src/domainModel.js
+++ src/domainModel.js
@@ -1,17 +1,28 @@
 export class DomainModel {
-  constructor({
-    boundaries = [0, 100],
-    value = boundaries,
-    step = null,
-    orientation = "horizontal",
-    tooltips = false,
-  } = {}) {
-    this.options = {
-      boundaries,
-      value,
-      step,
-      orientation,
-      tooltips,
+  // don't use object destructuring as
+  // I need to loop over an object
+  constructor(options = {}) {
+    // Assign default values directly in order
+    // to avoid their redundant pass through setters
+    this._options = {
+      boundaries: [0, 100],
+      step: null,
+      orientation: "horizontal",
+      tooltips: false,
     };
+
+    for (let key in this._options) {
+      if ( options[key] === undefined ) continue;
+      this[key] = options[key];
+    }
+
+    // if value isn't passed
+      // value is equal to half of the boundaries difference
+    // value is equal to options.value
+    this._options.value = (this._options.boundaries[1] - this._options.boundaries[0]) / 2;
+
+    if (options.value !== undefined) {
+      this.value = options.value;
+    }
   }
 }
