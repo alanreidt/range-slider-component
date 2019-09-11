975c2c9149a25948deecda274a9a843fd63337c5
Alan Reid <alanreidt@gmail.com>
2019-09-11 14:33:21 +0300

    Added tests for getOverstepOf function

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,203 @@
-import {getNearestDivisibleOf} from "../src/utilities";
+import {getOverstepOf, getNearestDivisibleOf} from "../src/utilities";
 import {initialization} from "./specs/initialization/initialization";
 import {reassignment} from "./specs/reassignment/reassignment";
+
+/**
+ * getOverstepOf TODO:
+ *  - shall return overstep
+ *    - overstep of 20 over 3,
+ *      starting from 0 is equal to 2
+ *    - overstep of 81 over 9,
+ *      starting from 0 is equal to 0
+ *  - shall take into account custom start
+ *    - overstep of 20 over 3,
+ *      starting from 2 is equal to 0
+ *    - overstep of -63 over 30,
+ *      starting from -154 is equal to 1
+ *  - shall take start as an initial step
+ *    - overstep of 10 over 50,
+ *      starting from 0 is equal to 10
+ *    - overstep of 10 over 50,
+ *      starting from 7 is equal to 3
+ *    - overstep of 0 over 50,
+ *      starting from 0 is equal to 0
+ *    - overstep of -220 over 50,
+ *      starting from -200 is equal to 20
+ *  - shall correct an input, if it's possible
+ *    - overstep of 110 over -50 is equal to 10
+ *  - shall return undefined if incorrect parameter is passed
+ *    - returns undefined if value less, than start
+ *      - overstep of -10 over 60,
+ *        starting from 0 is equal to undefined
+ *      - overstep of -63 over 30,
+ *        starting from -4 is equal to undefined
+ *    - returns undefined, if zero step is passed
+ *  - shall catch garbage input
+ *    - returns undefined, if parameters aren't passed
+ *    - returns undefined, if value is incorrect
+ *      - returns undefined, if value less, than start
+ *      - returns undefined, if value equals to undefined
+ *      - returns undefined, if value equals to null
+ *      - returns undefined, if value equals to Infinity
+ *      - returns undefined, if value equals to NaN
+ *      - returns undefined, if value equals to "text"
+ *      - returns undefined, if value equals to "123text"
+ *    - returns undefined, if step is incorrect
+ *      - returns undefined, if step equals to 0
+ *      - returns undefined, if step equals to undefined
+ *      - returns undefined, if step equals to null
+ *      - returns undefined, if step equals to Infinity
+ *      - returns undefined, if step equals to NaN
+ *      - returns undefined, if step equals to "text"
+ *      - returns undefined, if step equals to "123text"
+ *    - returns undefined, if start is incorrect
+ *      - returns undefined, if start equals to undefined
+ *      - returns undefined, if start equals to null
+ *      - returns undefined, if start equals to Infinity
+ *      - returns undefined, if start equals to NaN
+ *      - returns undefined, if start equals to "text"
+ *      - returns undefined, if start equals to "123text"
+ *
+ */
+
+describe("getOverstepOf", function() {
+
+  describe("shall return overstep", function() {
+
+    it(`overstep of 20 over 3 is equal to 2`, function() {
+      assert.deepEqual( getOverstepOf(20, 3), 2 );
+    });
+
+    it(`overstep of 81 over 9 is equal to 0`, function() {
+      assert.deepEqual( getOverstepOf(81, 9), 0 );
+    });
+
+  });
+
+
+  describe("shall take into account custom start", function() {
+
+    it(`overstep of 20 over 3, starting from 2 is equal to 0`, function() {
+      assert.deepEqual( getOverstepOf(20, 3, 2), 0 );
+    });
+
+    it(`overstep of -63 over 30, starting from -154 is equal to 1`, function() {
+      assert.deepEqual( getOverstepOf(-63, 30, -154), 1 );
+    });
+
+  });
+
+
+  describe("shall take start as an initial step", function() {
+
+    it(`overstep of 10 over 50, starting from 0 is equal to 10`, function() {
+      assert.deepEqual( getOverstepOf(10, 50), 10 );
+    });
+
+    it(`overstep of 10 over 50, starting from 7 is equal to 3`, function() {
+      assert.deepEqual( getOverstepOf(10, 50, 7), 3 );
+    });
+
+    it(`overstep of 0 over 50, starting from 0 is equal to 0`, function() {
+      assert.deepEqual( getOverstepOf(0, 50), 0 );
+    });
+
+    it(`overstep of -220 over 50, starting from -200 is equal to 20`, function() {
+      assert.deepEqual( getOverstepOf(-220, 50, -200), 20 );
+    });
+
+  });
+
+
+  describe("shall correct an input, if it's possible", function() {
+
+    context("corrects negative step", function() {
+      it("overstep of 110 over -50 is equal to 10", function() {
+        assert.deepEqual( getOverstepOf(110, -50), 10 );
+      });
+    });
+
+  });
+
+
+  describe("shall return undefined if incorrect parameter is passed", function() {
+
+    it("return undefined, if zero step is passed", function() {
+      assert.deepEqual( getOverstepOf(100, 0), undefined );
+    });
+
+    context("returns undefined if value less, than start", function() {
+      it(`overstep of -10 over 60, starting from 0 is equal to undefined`, function() {
+        assert.deepEqual( getOverstepOf(-10, 60), undefined );
+      });
+
+      it(`overstep of -63 over 30, starting from -4 is equal to undefined`, function() {
+        assert.deepEqual( getOverstepOf(-63, 30, -4), undefined );
+      });
+    });
+
+  });
+
+
+  describe("shall catch garbage input", function() {
+
+    it("return undefined, if parameters aren't passed", function() {
+      assert.deepEqual( getOverstepOf(), undefined );
+    });
+
+    context("returns undefined if value parameter is incorrect", function() {
+      let testValues = [undefined, null, Infinity, NaN, "text", "123text"];
+
+      testValues.forEach(testValue => {
+
+        it(`returns undefined, if value parameter equals to ${testValue}`, function() {
+          assert.deepEqual( getOverstepOf(testValue, 60), undefined );
+        });
+
+      });
+    });
+
+    context("returns undefined if step parameter is incorrect", function() {
+      let testValues = [undefined, null, Infinity, NaN, "text", "123text"];
+
+      testValues.forEach(testValue => {
+
+        it(`returns undefined, if step parameter equals to ${testValue}`, function() {
+          assert.deepEqual( getOverstepOf(100, testValue), undefined );
+        });
+
+      });
+    });
+
+    context("returns undefined if start parameter is incorrect", function() {
+      let testValues = [undefined, null, Infinity, NaN, "text", "123text"];
+
+      testValues.forEach(testValue => {
+
+        it(`returns undefined, if start parameter equals to ${testValue}`, function() {
+          assert.deepEqual( getOverstepOf(100, 60, testValue), undefined );
+        });
+
+      });
+    });
+
+  });
+
+
+  describe("should accept start or end, as a starting point", function() {
+
+    it(`overstep of -63 over 30, starting from 3 is equal to 0`, function() {
+      assert.deepEqual( getOverstepOf(-63, 30, 3), 14 );
+    });
+
+    it(`overstep of -63 over 30, starting from -4 is equal to 1`, function() {
+      assert.deepEqual( getOverstepOf(-63, 30, -4), 1 );
+    });
+
+  });
+
+});
+
 
 /**
  * getNearestDivisibleOf TODO:
