bb5c07cc8e4a1caf026312614055dccb4b292fa9
Alan Reid <alanreidt@gmail.com>
2019-09-13 13:33:57 +0300

    Extend test

--- tests/specs/reassignment/reassignmentCorrectionOfValue.js
+++ tests/specs/reassignment/reassignmentCorrectionOfValue.js
@@ -1,30 +1,25 @@
 import {Slider} from "../../../src/Slider";
 
 export function reassignmentCorrectionOfValue() {
-  let options = {
-    boundaries: [100, 500],
-    value: 180,
-    step: 20,
-    orientation: "vertical",
-    tooltips: true,
-  };
   let testValues = {
-    value: 190,
-  }
-  let testObject = {
-    value: 200,
+    boundaries: [ [0, 500], [-500, 500], [-1000, -500] ],
+    step: [100, 250, 50],
+    value: [190, -100, -525],
   };
-  let testSubject = new Slider(options);
+  let testObject = [200, 0, -500];
+  let testSubject = new Slider();
 
-  for (let key in testValues) {
-    testSubject[key] = testValues[key];
-  }
+  for (let i = 0; i < testObject.length; i++) {
+    for (let key in testValues) {
+      testSubject[key] = testValues[key][i];
+    }
 
-  for (let key in testObject) {
+    let testSubjectValue = testSubject.value;
 
-    it(`${key} is equal to ${testObject[key]}`, function() {
-      assert.deepEqual( testSubject[key], testObject[key] );
+    it(`if boundaries = ${testSubject.boundaries}, step = ${testSubject.step},
+    value = ${testSubject.value} were passed,
+    than corrects value to ${testObject[i]}`, function() {
+      assert.deepEqual( testSubjectValue, testObject[i] );
     });
-
   }
 }
