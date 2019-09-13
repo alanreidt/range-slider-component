6266553d031c2f45610cdd905a80cca2b26f34e4
Alan Reid <alanreidt@gmail.com>
2019-09-13 20:29:54 +0300

    Enhanced test structure

--- tests/specs/reassignment/reassignmentCorrectionOfValue.js
+++ tests/specs/reassignment/reassignmentCorrectionOfValue.js
@@ -1,36 +1,40 @@
 import {Slider} from "../../../src/Slider";
 
 export function reassignmentCorrectionOfValue() {
-  let testValues = {
-    boundaries: [ [0, 500], [-500, 500], [-1000, -500] ],
-    step: [100, 250, 50],
-    value: [190, -100, -525],
-  };
-  let testObject = {
-    value: [200, 0, -500],
-  };
+  let testValueEntries = [
+    {boundaries: [0, 500], step: 100, value: 190},
+    {boundaries: [-500, 500], step: 250, value: -100},
+    {boundaries: [-1000, -500], step: 50, value: -525},
+  ];
+  let testObjectEntries = [
+    {value: 200},
+    {value: 0},
+    {value: -500},
+  ];
   let testSubject = new Slider();
-  let testValuesKeys = Object.keys(testValues);
-  let testObjectKeys = Object.keys(testObject);
+  // let testValuesKeys = Object.keys(testValues);
+  // let testObjectKeys = Object.keys(testObject);
 
-  testObjectKeys.forEach(testObjectKey => {
-    let testObjectValue = testObject[testObjectKey];
+  testObjectEntries.forEach( (testObjectEntry, index) => {
 
-    for (let i = 0; i < testObjectValue.length; i++) {
-      let testValuesRecord = '';
+    // let testObjectValue = testObject[testObjectKey];
 
-      testValuesKeys.forEach(key => {
-        let testValuesValue = testValues[key];
+    for (let testObjectEntryKey in testObjectEntry) {
+      let testValueEntry = testValueEntries[index];
+      let testValueEntryRecord = '';
 
-        testSubject[key] = testValuesValue[i];
-        testValuesRecord += `${key} = ${testValuesValue[i]}, `;
-      });
+      for (let key in testValueEntry) {
+        // let testValuesValue = testValues[key];
 
-      let testSubjectValue = testSubject[testObjectKey];
+        testSubject[key] = testValueEntry[key];
+        testValueEntryRecord += `${key} = ${testValueEntry[key]}, `;
+      };
 
-      it(`if ${testValuesRecord} were passed,
-      than corrects value to ${testObjectValue[i]}`, function() {
-        assert.deepEqual( testSubjectValue, testObjectValue[i] );
+      let testSubjectValue = testSubject[testObjectEntryKey];
+
+      it(`if ${testValueEntryRecord} were passed,
+      than corrects value to ${testObjectEntry[testObjectEntryKey]}`, function() {
+        assert.deepEqual( testSubjectValue, testObjectEntry[testObjectEntryKey] );
       });
     }
 
