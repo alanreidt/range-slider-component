1e68070593eb0732bde494912ea137ff7aadc67f
Alan Reid <alanreidt@gmail.com>
2019-09-17 12:34:02 +0300

    Corrected isValueInBetween function tests

--- tests/test.js
+++ tests/test.js
@@ -27,8 +27,11 @@ describe("isValueInBetween", function() {
       [-50, -100, 0],
       [-250, -300, -100],
       [0, -300, 300],
+      [0, -300, Infinity],
+      [0, -Infinity, 300],
+      [1000000000, -Infinity, Infinity],
     ];
-    let expectations = [true, true, true, true];
+    let expectations = new Array(funcOptions.length).fill(true);
 
     testCurrent(funcOptions, expectations);
   });
@@ -38,9 +41,11 @@ describe("isValueInBetween", function() {
       [150, 0, 100],
       [-150, -100, 0],
       [-350, -300, -100],
-      [500, -300, 300],
+      [-500, -300, 300],
+      [-500, -300, Infinity],
+      [500, -Infinity, 300],
     ];
-    let expectations = [false, false, false, false];
+    let expectations = new Array(funcOptions.length).fill(false);
 
     testCurrent(funcOptions, expectations);
   });
@@ -51,8 +56,11 @@ describe("isValueInBetween", function() {
       [-50, 0, -100],
       [-250, -100, -300],
       [0, 300, -300],
+      [0, Infinity, -300],
+      [0, 300, -Infinity],
+      [1000000000, Infinity, -Infinity],
     ];
-    let expectations = [true, true, true, true];
+    let expectations = new Array(funcOptions.length).fill(true);
 
     testCurrent(funcOptions, expectations);
   });
@@ -61,8 +69,14 @@ describe("isValueInBetween", function() {
     let funcOptions = [
       [100, 0, 100],
       [0, 0, 100],
+      [300, -300, 300],
+      [-300, -300, 300],
+      [Infinity, -300, Infinity],
+      [-Infinity, -Infinity, 300],
+      [Infinity, -Infinity, Infinity],
+      [-Infinity, -Infinity, Infinity],
     ];
-    let expectations = [false, false];
+    let expectations = new Array(funcOptions.length).fill(false);
 
     testCurrent(funcOptions, expectations);
   });
@@ -71,9 +85,12 @@ describe("isValueInBetween", function() {
     let funcOptions = [
       [100, 100, 100],
       [100, 100, 101],
-      [101, 100, 102],
+      [101, 100, 101],
+      [-300, -300, -300],
+      [-300, -301, -300],
+      [-301, -301, -300],
     ];
-    let expectations = [false, false, true];
+    let expectations = new Array(funcOptions.length).fill(false);
 
     testCurrent(funcOptions, expectations);
   });
@@ -81,45 +98,40 @@ describe("isValueInBetween", function() {
   context("shall catch garbage input", function() {
     describe("returns false, if value is incorrect", function() {
       let funcOptions = [
-        [undefined, 0, 100],
-        [null, 0, 100],
-        [Infinity, 0, 100],
-        [NaN, 0, 100],
-        ["text", 0, 100],
-        ["123text", 0, 100],
-        [undefined, 0, 100],
+        [undefined, -100, 100],
+        [null, -100, 100],
+        [Infinity, -100, 100],
+        [NaN, -100, 100],
+        ["text", -100, 100],
+        ["123text", -100, 100],
       ];
-      let expectations = new Array(6).fill(false);
+      let expectations = new Array(funcOptions.length).fill(false);
 
       testCurrent(funcOptions, expectations);
     });
 
     describe("returns false, if start is incorrect", function() {
       let funcOptions = [
-        [0, undefined, 100],
-        [0, null, 100],
-        [0, Infinity, 100],
-        [0, NaN, 100],
-        [0, "text", 100],
-        [0, "123text", 100],
-        [0, undefined, 100],
+        [50, undefined, 100],
+        [50, null, 100],
+        [50, NaN, 100],
+        [50, "text", 100],
+        [50, "123text", 100],
       ];
-      let expectations = new Array(6).fill(false);
+      let expectations = new Array(funcOptions.length).fill(false);
 
       testCurrent(funcOptions, expectations);
     });
 
     describe("returns false, if end is incorrect", function() {
       let funcOptions = [
-        [0, 0, undefined],
-        [0, 0, null],
-        [0, 0, Infinity],
-        [0, 0, NaN],
-        [0, 0, "text"],
-        [0, 0, "123text"],
-        [0, 0, undefined],
+        [50, 100, undefined],
+        [50, 100, null],
+        [50, 100, NaN],
+        [50, 100, "text"],
+        [50, 100, "123text"],
       ];
-      let expectations = new Array(6).fill(false);
+      let expectations = new Array(funcOptions.length).fill(false);
 
       testCurrent(funcOptions, expectations);
     });
