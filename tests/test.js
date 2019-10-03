19cb509e3c2dea2f90812ac32e9d48952a097684
Alan Reid <alanreidt@gmail.com>
2019-10-03 19:54:37 +0300

    Correct the createBase function

--- tests/test.js
+++ tests/test.js
@@ -865,11 +865,12 @@ describe("ViewController", function() {
   //  setHandlePosition (handle, position)
   //    shall set position of handle
 
-  describe("createBase method", function() {
+  describe("createBase function", function() {
     it("shall create base of the slider", function() {
-      let base = createBase(div, [100, 500]);
+      let base = createBase();
 
-      assert.equal(base.className, "slider__base");
+      assert.isDefined(handle);
+      assert.equal(base.className.contains("slider__base"), true);
     });
   });
 
