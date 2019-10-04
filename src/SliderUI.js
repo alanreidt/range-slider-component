14cab33b651f7c2b7f99f1547145ad980c570618
Alan Reid <alanreidt@gmail.com>
2019-10-04 14:44:34 +0300

    Add updateSliderUI method

--- src/SliderUI.js
+++ src/SliderUI.js
@@ -1,2 +1,16 @@
 export class SliderUI {
+
+  updateSliderUI({boundaries, values, step, orientation, tooltipsState} = {}) {
+
+    if (values) {
+      values.forEach( (value, i) => {
+        let handlePosition = getPositionInPercentageOf(value, boundaries);
+        let handle = this.handles[i];
+
+        handle.style.transform = `translate3d(${handlePosition}, 0, 0)`;
+      });
+    }
+
+  }
+
 }
