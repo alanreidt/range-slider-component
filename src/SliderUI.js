c072ac4054b2ed9db90c411bb92ebf625def17e6
Alan Reid <alanreidt@gmail.com>
2019-10-04 17:12:01 +0300

    Add createSliderUI method

--- src/SliderUI.js
+++ src/SliderUI.js
@@ -1,4 +1,30 @@
+import {getPositionInPercentageOf, createBase, createHandle, createTooltip} from "./utilities";
+
 export class SliderUI {
+
+  createSliderUI({boundaries, values, step, orientation, tooltipsState} = {}) {
+    let base = createBase();
+    this.handles = [];
+    this.tooltips = [];
+
+    values.forEach( (value) => {
+      let handlePosition = getPositionInPercentageOf(value, boundaries);
+      let handle = createHandle(handlePosition);
+      let tooltip = null;
+
+      if (tooltipsState) {
+        tooltip = createTooltip(value);
+        this.tooltips.push(tooltip);
+
+        handle.append(tooltip);
+      }
+
+      this.handles.push(handle);
+      base.append(handle);
+    });
+
+    this.sliderUI = base;
+  }
 
   updateSliderUI({boundaries, values, step, orientation, tooltipsState} = {}) {
 
