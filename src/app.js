437bc7a2a68487d70d393396c598b64b8afeb783
Alan Reid <alanreidt@gmail.com>
2019-10-06 12:55:16 +0300

    Set up Slider for prototype testing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
-import {Slider} from "/Slider";
+import {Slider} from "./Slider";
+import {Model} from "./Model";
+import {SliderUI} from "./SliderUI";
 
-let slider = new Slider();
-console.log("Hello!");
-console.log(slider);
+let slider = new Slider({
+  value: [0, 100],
+  tooltips: true,
+});
+let model = new Model(slider);
+let sliderui = new SliderUI();
+let sliderElement = document.querySelector(".slider");
+
+sliderui.create( model.getValues() );
+sliderElement.append(sliderui.sliderUI);
