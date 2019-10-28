acc93d28b46f73ad30ade88b3f6573c5ad99526c
Alan Reid <alanreidt@gmail.com>
2019-10-28 18:43:11 +0300

    Refactor Template class

--- src/Template.js
+++ src/Template.js
@@ -3,17 +3,17 @@ export class Template {
   constructor({boundaries, values, step, orientation, hasTooltips} = {}) {
     const positions = values.map( (value) => getPositionInPercentageOf(value, boundaries) );
 
-    const handleGroups = positions.reduce( (str, position, i) => {
-      const value = values[i];
-
-      str +
-      `<div class="slider__origin" style="left: ${position}">
-        ${hasTooltips ? `<div class="slider__tooltip">${value}</div>` : ''}
-        <div class="slider__handle"></div>
+    return(
+      `<div class="slider__base">
+        ${positions.reduce( (str, position, i) => {
+          str +
+          `<div class="slider__handle-group" style="left: ${position}">
+            ${hasTooltips ? `<div class="slider__tooltip">${value}</div>` : ''}
+            <div class="slider__handle"></div>
+          </div>`;
+        }, '')}
       </div>`
-    }, '');
-
-    return `<div class="slider__base">${handleGroups}</div>`;
+    );
   }
 
 }
