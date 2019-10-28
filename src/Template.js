7cb275a41b1d484c7615cc6e0e2e6b35f66e4265
Alan Reid <alanreidt@gmail.com>
2019-10-28 21:23:08 +0300

    Correct Template class

--- src/Template.js
+++ src/Template.js
@@ -1,14 +1,14 @@
 export class Template {
 
-  constructor({boundaries, values, step, orientation, hasTooltips} = {}) {
+  createDefault({boundaries, values, hasTooltips} = {}) {
     const positions = values.map( (value) => getPositionInPercentageOf(value, boundaries) );
 
     return(
       `<div class="slider__base">
         ${positions.reduce( (str, position, i) => {
-          str +
+          return str +
           `<div class="slider__handle-group" style="left: ${position}">
-            ${hasTooltips ? `<div class="slider__tooltip">${value}</div>` : ''}
+            ${hasTooltips ? `<div class="slider__tooltip">${values[i]}</div>` : ''}
             <div class="slider__handle"></div>
           </div>`;
         }, '')}
