70df793894c6d2dc10891bff20a8e3b654d51811
Alan Reid <alanreidt@gmail.com>
2019-10-29 17:53:00 +0300

    Activate SliderUI update method

--- src/app.js
+++ src/app.js
@@ -11,3 +11,8 @@ let model = new Model(slider);
 let sliderElement = document.querySelector(".someUserClass");
 let template = new Template();
 let sliderUI = new SliderUI(sliderElement, model, template);
+
+sliderUI.update({
+  boundaries: [0, 100],
+  values: [30, 70],
+});
