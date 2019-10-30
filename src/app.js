9ddea31fe62a198b002baab29771266609c8bf82
Alan Reid <alanreidt@gmail.com>
2019-10-30 21:10:59 +0300

    Edit app.js using Model's observerMixin capabilities

--- src/app.js
+++ src/app.js
@@ -8,11 +8,20 @@ let slider = new Slider({
   hasTooltips: true,
 });
 let model = new Model(slider);
+
 let sliderElement = document.querySelector(".someUserClass");
 let template = new Template();
-let sliderUI = new SliderUI(sliderElement, model, template);
+let sliderUi = new SliderUI(sliderElement, template);
 
-sliderUI.update({
+const sliderUiBoundedPaint = sliderUi.paint.bind(sliderUi);
+const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
+
+model.addSubscriber( "create", sliderUiBoundedPaint );
+model.addSubscriber( "update", sliderUiBoundedUpdate );
+
+model.triggerSubscribers( "create", model.getValues() );
+
+model.triggerSubscribers("update", {
   boundaries: [0, 100],
   values: [30, 70],
 });
