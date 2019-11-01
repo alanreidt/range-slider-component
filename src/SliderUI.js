6f72f09434e1e1ba90b1fc681c85d7c5be677036
Alan Reid <alanreidt@gmail.com>
2019-11-01 19:55:54 +0300

    Move Template into SliderUI

--- src/SliderUI.js
+++ src/SliderUI.js
@@ -40,6 +40,25 @@ export class SliderUI {
   }
 
 
+  _createTemplate({boundaries, values, step, orientation, hasTooltips} = {}) {
+    const positions = values.map( (value) => getPositionInPercentageOf(value, boundaries) );
+
+    return(
+      `<div class="slider ${(orientation === "vertical") ? "slider_vertical" : ''}">
+        <div class="slider__base">
+          ${positions.reduce( (str, position, i) => {
+            return str +
+            `<div class="slider__handle-group" style="left: ${position}">
+              ${hasTooltips ? `<div class="slider__tooltip">${values[i]}</div>` : ''}
+              <div class="slider__handle"></div>
+            </div>`;
+          }, '')}
+        </div>
+      </div>`
+    );
+  }
+
+
   _updateHandleGroups(positions) {
     const handleGroups = this._getHandleGroups();
 
