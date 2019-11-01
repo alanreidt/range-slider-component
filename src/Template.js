7ca04086b1491f285b6f491e33907a6a863b7e09
Alan Reid <alanreidt@gmail.com>
2019-11-01 19:55:05 +0300

    Correct Template createDefault method

--- src/Template.js
+++ src/Template.js
@@ -2,18 +2,20 @@ import { getPositionInPercentageOf } from "./utilities";
 
 export class Template {
 
-  createDefault({boundaries, values, hasTooltips} = {}) {
+  createDefault({boundaries, values, step, orientation, hasTooltips} = {}) {
     const positions = values.map( (value) => getPositionInPercentageOf(value, boundaries) );
 
     return(
-      `<div class="slider__base">
-        ${positions.reduce( (str, position, i) => {
-          return str +
-          `<div class="slider__handle-group" style="left: ${position}">
-            ${hasTooltips ? `<div class="slider__tooltip">${values[i]}</div>` : ''}
-            <div class="slider__handle"></div>
-          </div>`;
-        }, '')}
+      `<div class="slider ${(orientation === "vertical") ? "slider_vertical" : ''}">
+        <div class="slider__base">
+          ${positions.reduce( (str, position, i) => {
+            return str +
+            `<div class="slider__handle-group" style="left: ${position}">
+              ${hasTooltips ? `<div class="slider__tooltip">${values[i]}</div>` : ''}
+              <div class="slider__handle"></div>
+            </div>`;
+          }, '')}
+        </div>
       </div>`
     );
   }
