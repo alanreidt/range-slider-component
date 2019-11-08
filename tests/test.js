f5da0e1ac6ef130e414de31f29044114d0e07d61
Alan Reid <alanreidt@gmail.com>
2019-11-08 14:03:00 +0300

    Realize SliderUI update method test

--- tests/test.js
+++ tests/test.js
@@ -1056,21 +1056,38 @@ describe("SliderUI", function() {
 
 
   describe("update method", function() {
-    it("shall set values", function() {
+    const parent = document.createElement("div");
+    const sliderUi = new SliderUI(parent);
+    const options = {
+      boundaries: [0, 100],
+      values: [20, 80],
+      step: 1,
+      orientation: "vertical",
+      hasTooltips: true,
+    };
+
+    sliderUi.paint(options);
+
+    describe("shall set values", function() {
+
+      context(`${SLIDER_HANDLE_GROUP_NAME}s are set`, function() {
+        const handleGroups = Array.from( parent.querySelectorAll(`.${SLIDER_HANDLE_GROUP_NAME}`) );
+
+        handleGroups.forEach( (handleGroup, i) => {
+          const value = options.values[i];
+          const regexp = new RegExp(`${value}`);
+          const handleGroupStyle = handleGroup.getAttribute("style");
+
+          it(`${SLIDER_HANDLE_GROUP_NAME}${i + 1} equals to ${value}`, function() {
+            assert.isNotNull( handleGroupStyle.match(regexp) );
+          });
+        });
+      });
+
     });
 
     describe("shall update values", function() {
-      const parent = document.createElement("div");
-      const sliderUi = new SliderUI(parent);
       const newValues = [10, 60];
-
-      sliderUi.paint({
-        boundaries: [0, 100],
-        values: [20, 80],
-        step: 1,
-        orientation: "vertical",
-        hasTooltips: true,
-      });
 
       sliderUi.update({
         boundaries: [0, 100],
@@ -1091,6 +1108,7 @@ describe("SliderUI", function() {
         });
       });
     });
+
   });
 
 });
