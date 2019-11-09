ffe3383163bf4fe0e287939f150ce86413fe2e3a
Alan Reid <alanreidt@gmail.com>
2019-11-09 20:14:27 +0300

    Correct indents in app.js

--- src/app.js
+++ src/app.js
@@ -12,8 +12,7 @@ let $parent = document.querySelector(".someUserClass");
 let sliderUi = new SliderUI($parent, model);
 
 const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
-
-model.addSubscriber( "update", sliderUiBoundedUpdate );
+model.addSubscriber("update", sliderUiBoundedUpdate);
 
 model.triggerSubscribers("update", {
   boundaries: [0, 100],
