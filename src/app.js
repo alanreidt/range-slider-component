e9df97dbbb8ebf30d783dbb3387e23ce309c4660
Alan Reid <alanreidt@gmail.com>
2019-11-09 20:09:26 +0300

    Remove redundant code in app.js

--- src/app.js
+++ src/app.js
@@ -11,13 +11,9 @@ let model = new Model(slider);
 let sliderElement = document.querySelector(".someUserClass");
 let sliderUi = new SliderUI(sliderElement);
 
-const sliderUiBoundedPaint = sliderUi.paint.bind(sliderUi);
 const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
 
-model.addSubscriber( "create", sliderUiBoundedPaint );
 model.addSubscriber( "update", sliderUiBoundedUpdate );
-
-model.triggerSubscribers( "create", model.getValues() );
 
 model.triggerSubscribers("update", {
   boundaries: [0, 100],
