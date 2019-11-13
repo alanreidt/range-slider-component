8363d67d30bf230e856ce8ef49e4f72e098e851b
Alan Reid <alanreidt@gmail.com>
2019-11-13 20:27:20 +0300

    Create createSlider function in app.js for now

--- src/app.js
+++ src/app.js
@@ -2,14 +2,19 @@ import {Slider} from "./Slider";
 import {Model} from "./Model";
 import {SliderUI} from "./SliderUI";
 
-const slider = new Slider({
+const $parent = document.querySelector(".user-slider");
+
+createSlider($parent, {
   values: [0, 100],
   hasTooltips: true,
 });
-const model = new Model(slider);
 
-const $parent = document.querySelector(".user-slider");
-const sliderUi = new SliderUI($parent, model);
 
-const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
-model.addSubscriber("update", sliderUiBoundedUpdate);
+function createSlider($parent, options) {
+  const slider = new Slider(options);
+  const model = new Model(slider);
+  const sliderUi = new SliderUI($parent, model);
+
+  const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
+  model.addSubscriber("update", sliderUiBoundedUpdate);
+}
