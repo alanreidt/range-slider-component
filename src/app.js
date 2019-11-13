3362d7a8a2a6ffa65f62015ed56306e27e39d06c
Alan Reid <alanreidt@gmail.com>
2019-11-13 17:58:00 +0300

    Replace "let" variables with "cont" in app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import {Slider} from "./Slider";
 import {Model} from "./Model";
 import {SliderUI} from "./SliderUI";
 
-let slider = new Slider({
+const slider = new Slider({
   values: [0, 100],
   hasTooltips: true,
 });
-let model = new Model(slider);
+const model = new Model(slider);
 
-let $parent = document.querySelector(".user-slider");
-let sliderUi = new SliderUI($parent, model);
+const $parent = document.querySelector(".user-slider");
+const sliderUi = new SliderUI($parent, model);
 
 const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
 model.addSubscriber("update", sliderUiBoundedUpdate);
