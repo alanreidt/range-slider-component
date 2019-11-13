a2f8588b47a59a140b94d00263830f5a1eeac1bf
Alan Reid <alanreidt@gmail.com>
2019-11-13 20:43:48 +0300

    Create vertical slider in app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,18 @@ import {Model} from "./Model";
 import {SliderUI} from "./SliderUI";
 
 const $parent = document.querySelector(".user-slider");
+const $parentVertical = document.querySelector(".user-slider-vertical");
 
 createSlider($parent, {
   values: [0, 100],
+  hasTooltips: true,
+});
+
+createSlider($parentVertical, {
+  boundaries: [200, 1000],
+  values: [300, 800],
+  step: 100,
+  orientation: "vertical",
   hasTooltips: true,
 });
 
