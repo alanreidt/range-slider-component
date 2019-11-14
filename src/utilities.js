eccdeabc8ba79df78931dfb27aad773078edca3a
Alan Reid <alanreidt@gmail.com>
2019-11-14 12:54:06 +0300

    Add flag parameter to call utility function

--- src/utilities.js
+++ src/utilities.js
@@ -35,14 +35,15 @@ export function setElementsTextContent(elements, values) {
  * of the array.
  * @param {array} values The values, to be passed to the callback
  * on each iteration.
+ * @param {any} flag The argument, which is the same for each callback call.
  */
 
-export function call(callback, values) {
+export function call(callback, values, flag) {
 
   return (element, i) => {
     const value = values[i];
 
-    callback(element, value);
+    callback(element, value, flag);
   }
 
 }
