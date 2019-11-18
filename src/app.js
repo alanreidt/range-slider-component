c4a6e1b4d33168a044e02862f01da1c339b3d93e
Alan Reid <alanreidt@gmail.com>
2019-11-18 23:21:39 +0300

    Connect SliderAPI to app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+import {SliderAPI} from "./SliderAPI";
+
 const $parent = document.querySelector(".user-slider");
 const $parentVertical = document.querySelector(".user-slider-vertical");
 
-createSlider($parent, {
+SliderAPI.createSlider($parent, {
   values: [0, 100],
   hasTooltips: true,
 });
 
-createSlider($parentVertical, {
+SliderAPI.createSlider($parentVertical, {
   boundaries: [200, 1000],
   values: [300, 800],
   step: 100,
