c6892a8001addcd63efd9d805b528583d4152e12
Alan Reid <alanreidt@gmail.com>
2019-11-18 22:50:51 +0300

    Move slider creation functionality from app.js into SliderAPI class

--- src/SliderAPI.js
+++ src/SliderAPI.js
@@ -1,1 +1,15 @@
+import {Slider} from "./Slider";
+import {Model} from "./Model";
+import {SliderUI} from "./SliderUI";
+
+
 export class SliderAPI {}
+
+function createSlider($parent, options) {
+  const slider = new Slider(options);
+  const model = new Model(slider);
+  const sliderUi = new SliderUI($parent, model);
+
+  const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
+  model.addSubscriber("update", sliderUiBoundedUpdate);
+}
--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-import {Slider} from "./Slider";
-import {Model} from "./Model";
-import {SliderUI} from "./SliderUI";
-
 const $parent = document.querySelector(".user-slider");
 const $parentVertical = document.querySelector(".user-slider-vertical");
 
@@ -17,13 +13,3 @@ createSlider($parentVertical, {
   orientation: "vertical",
   hasTooltips: true,
 });
-
-
-function createSlider($parent, options) {
-  const slider = new Slider(options);
-  const model = new Model(slider);
-  const sliderUi = new SliderUI($parent, model);
-
-  const sliderUiBoundedUpdate = sliderUi.update.bind(sliderUi);
-  model.addSubscriber("update", sliderUiBoundedUpdate);
-}
