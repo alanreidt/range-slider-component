0f2055b3c469c32609d2a7cd7ac26e4ff7309ee8
Alan Reid <alanreidt@gmail.com>
2019-11-18 17:59:54 +0300

    Fix Slider initialization test according to changes in testClass test utility function

--- tests/specs/initialization/initialization.js
+++ tests/specs/initialization/initialization.js
@@ -2,17 +2,47 @@ import {initializationWithDefaults} from "./initializationWithDefaults";
 import {initializationWithPartDefaults} from "./initializationWithPartDefaults";
 import {initializationWithIncorrectFixed} from "./initializationWithIncorrectFixed";
 import {initializationWithMixedInputFixed} from "./initializationWithMixedInputFixed";
+import { testClass } from "../../testUtilities";
+import { Slider } from "../../../src/Slider";
 
 export function initialization() {
+  const Class = Slider;
+  const methodGetter = "getValues";
+
+  const runTest = testClass({Class, methodGetter});
 
   describe("shall assign default values for unpassed parameters", function () {
 
     context("shall assign default values, if nothing is passed", function() {
-      initializationWithDefaults();
+      const expectations = [{
+        boundaries: [0, 100],
+        values: [50],
+        step: 1,
+        orientation: "horizontal",
+        hasTooltips: false,
+      }];
+      const testOptions = {expectations};
+
+      runTest(testOptions);
     });
 
     context("shall assign default values for unpassed parameters, if only part is passed", function() {
-      initializationWithPartDefaults();
+      const ClassOptions = {
+        boundaries: [5000, 40000],
+        values: 20000,
+        step: 50,
+        orientation: "vertical",
+      };
+      const expectations = [{
+        boundaries: [5000, 40000],
+        values: [20000],
+        step: 50,
+        orientation: "vertical",
+        hasTooltips: false,
+      }];
+      const testOptions = {ClassOptions, expectations};
+
+      runTest(testOptions);
     });
 
   });
@@ -21,17 +51,49 @@ export function initialization() {
   describe("shall assign fixed values for incorrect arguments", function() {
 
     context("shall assign default value if incorrect one is passed", function() {
-      initializationWithIncorrectFixed();
+      const ClassOptions = {
+        boundaries: ["hundred", 200],
+        values: [25, "p100"],
+        step: -20,
+        orientation: "right",
+        hasTooltips: 7,
+      };
+      const expectations = [{
+        boundaries: [0, 200],
+        values: [25],
+        step: 1,
+        orientation: "horizontal",
+        hasTooltips: false,
+      }];
+      const testOptions = {ClassOptions, expectations};
+
+      runTest(testOptions);
     });
 
     context("shall parse number from mixed inputs", function() {
-      initializationWithMixedInputFixed();
+      const ClassOptions = {
+        boundaries: [0, "1000px"],
+        values: ["0$", 600],
+        step: "100",
+      };
+      const expectations = [{
+        boundaries: [0, 1000],
+        values: [0, 600],
+        step: 100,
+        orientation: "horizontal",
+        hasTooltips: false,
+      }];
+      const testOptions = {ClassOptions, expectations};
+
+      runTest(testOptions);
     });
 
   });
 
 
   describe(`shall accept array of {value} values`, function() {
+
+    it(`shall limit array of {value} to 8 values`);
 
     context(`shall accept array of {value} values`, function() {
       const ClassOptions = {
@@ -65,8 +127,6 @@ export function initialization() {
       runTest(testOptions);
     });
 
-    it(`shall limit array of {value} to 8 values`);
-
   });
 
 
@@ -80,7 +140,7 @@ export function initialization() {
     const expectations = [
       { values: [200] },
     ];
-    const testOptions = {ClassOptions, options, expectations};
+    const testOptions = {ClassOptions, expectations};
 
     runTest(testOptions);
   });
