661a735d85429467d8c652471b96b4eff6c58858
Alan Reid <alanreidt@gmail.com>
2019-11-22 14:27:05 +0300

    Remove vertical slider creation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 import {SliderAPI} from "./SliderAPI";
 
 const $parent = document.querySelector(".user-slider");
-const $parentVertical = document.querySelector(".user-slider-vertical");
 
 SliderAPI.createSlider($parent, {
   values: [0, 100],
-  hasTooltips: true,
-});
-
-SliderAPI.createSlider($parentVertical, {
-  boundaries: [200, 1000],
-  values: [300, 800],
-  step: 100,
-  orientation: "vertical",
   hasTooltips: true,
 });
