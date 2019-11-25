a3e10f23a5000f973122327d7ac2172eab610c3f
Alan Reid <alanreidt@gmail.com>
2019-11-25 22:17:48 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import {slider} from "../slider/slider.js";
+
+const $parent = document.querySelector(".slider-wrapper");
+
+const $form = document.querySelector(".js-control-pane__form");
+const $boundariesTextInput = document.querySelector("#boundaries");
+const $valuesTextInput = document.querySelector("#values");
+const $stepNumberInput = document.querySelector("#step");
+const $orientationCheckbox = document.querySelector("#orientation");
+const $hasTooltipsCheckbox = document.querySelector("#hasTooltips");
+
+slider.create($parent, {
+  boundaries: [0, 100],
+  values: [20, 80],
+  hasTooltips: true,
+});
+
+$form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const boundaries = $boundariesTextInput.value.split(", ");
+  const values = $valuesTextInput.value.split(", ");
+  const step = $stepNumberInput.valueAsNumber;
+  const orientation = $orientationCheckbox.checked ? "vertical" : "horizontal";
+  const hasTooltips = $hasTooltipsCheckbox.checked;
+
+  const options = {
+    boundaries,
+    values,
+    step,
+    hasTooltips,
+    orientation,
+  };
+
+  slider.create($parent, options);
+});
