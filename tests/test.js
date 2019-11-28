c9c698682e28dada796bae7f074856ea0736f6d0
Alan Reid <alanreidt@gmail.com>
2019-11-28 20:25:11 +0300

    Export getNearestTo function test into according folder

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,6 @@ import {
   getOverstepOf,
   getNearestDivisibleOf,
   isValueBetween,
-  getNearestTo,
   getPositionInPercentageOf,
   setElementPosition,
   setElementsPosition,
@@ -15,79 +14,10 @@ import { SliderAdapter } from "../src/SliderAdapter";
 import { SliderUI } from "../src/SliderUI";
 import { slider } from "../src/slider.js";
 import { testGetClosestFactorOf } from "../src/utilities/getClosestFactorOf/getClosestFactorOf.test";
+import { testGetNearestTo } from "../src/utilities/getNearestTo/getNearestTo.test.js";
 
 testGetClosestFactorOf();
-
-describe("getNearestTo", function() {
-  let describeTest = template`nearest number to ${0} from ${"...rest"} is ${"expectation"}`;
-  let testCurrent = test(getNearestTo, describeTest);
-
-  describe("shall return nearest number to value", function() {
-    let funcOptions = [
-      [100, 50, 90],
-      [-100, -50, -90],
-      [0, -50, 90],
-      [250, -100, 100],
-      [250, -100, 100, 250],
-      [100, 50, 90, 105],
-    ];
-    let expectations = [90, -90, -50, 100, 250, 105];
-
-    testCurrent(funcOptions, expectations);
-  });
-
-  describe("shall return last nearest number to value, if controvertial", function() {
-    let funcOptions = [
-      [100, 99, 105, 101, 5],
-      [100, 101, 105, 99, 5],
-    ];
-    let expectations = [101, 99];
-
-    testCurrent(funcOptions, expectations);
-  });
-
-  describe("shall filter incorrect arguments", function() {
-    let funcOptions = [
-      [100, null, 105, undefined, 5],
-      [0, null, 105, Infinity, 5],
-    ];
-    let expectations = [105, 5];
-
-    testCurrent(funcOptions, expectations);
-  });
-
-  context("shall catch garbage input", function() {
-    describe("returns NaN if value parameter is incorrect", function() {
-      let funcOptions = [
-        [undefined, -100, 100],
-        [null, -100, 100],
-        [Infinity, -100, 100],
-        [NaN, -100, 100],
-        ["text", -100, 100],
-        ["123text", -100, 100],
-      ];
-      let expectations = new Array(funcOptions.length).fill(NaN);
-
-      testCurrent(funcOptions, expectations);
-    });
-
-    describe("returns NaN if args parameter is incorrect", function() {
-      let funcOptions = [
-        [50, undefined],
-        [50, null],
-        [50, Infinity],
-        [50, NaN],
-        [50, "text"],
-        [50, "123text"],
-      ];
-      let expectations = new Array(funcOptions.length).fill(NaN);
-
-      testCurrent(funcOptions, expectations);
-    });
-  });
-
-});
-
+testGetNearestTo();
 
 describe("isValueBetween", function() {
   let describeTest = template`${0} between ${1} and ${2} is equal to ${"expectation"}`;
