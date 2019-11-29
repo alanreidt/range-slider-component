f3f2ce04ad74a33f1f817321384accd1daad08c4
Alan Reid <alanreidt@gmail.com>
2019-11-30 00:15:11 +0300

    Refactor packInto utility function

--- src/utilities/packInto/packInto.js
+++ src/utilities/packInto/packInto.js
@@ -2,5 +2,9 @@ import { isValueInBetween } from "../isValueInBetween/isValueInBetween";
 import { getNearestTo } from "../getNearestTo/getNearestTo";
 
 export function packInto(value, start, end) {
-  return isValueInBetween(value, start, end) ? value : getNearestTo(value, start, end);
+
+  return isValueInBetween(value, start, end) ?
+    value :
+    getNearestTo(value, start, end);
+
 }
