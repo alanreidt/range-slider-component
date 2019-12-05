04de5eacbfa09fe7e4ce87b9822e4edd5b94e8dd
Alan Reid <alanreidt@gmail.com>
2019-12-06 00:25:49 +0300

    Modify translateProportionIntoValue function Add proportion overflow handling logic

--- src/utilities/translateProportionIntoValue/translateProportionIntoValue.js
+++ src/utilities/translateProportionIntoValue/translateProportionIntoValue.js
@@ -1,10 +1,13 @@
+import { isValueInBetween } from "../isValueInBetween/isValueInBetween";
+
+
 export function translateProportionIntoValue(proportion, range) {
   const inputValues = [proportion].concat(range);
   const isIncorrect = inputValues.some(
     (value) => !Number.isFinite(value)
   );
 
-  if ( isIncorrect ) {
+  if ( isIncorrect || !isValueInBetween(proportion, 0 , 100) ) {
     return NaN;
   }
 
