dd7898f9d0eb9f0a6b6c9fdc5fd52d89c715aa9e
Alan Reid <alanreidt@gmail.com>
2019-12-05 23:42:46 +0300

    Modify translateProportionIntoValue function Correct indents

--- src/utilities/translateProportionIntoValue/translateProportionIntoValue.js
+++ src/utilities/translateProportionIntoValue/translateProportionIntoValue.js
@@ -1,9 +1,13 @@
 export function translateProportionIntoValue(proportion, range) {
   let [start, end] = range;
+
   if (start > end) {
     [start, end] = [end, start];
   }
+
   const difference = end - start;
+
   proportion = proportion / 100;
+
   return difference * proportion + start;
 }
