64c8cd8cb12d837b451cb01e63f455f20ee8aef4
Alan Reid <alanreidt@gmail.com>
2019-12-19 22:57:36 +0300

    Modify SliderModel Eslint corrections

--- src/SliderModel.js
+++ src/SliderModel.js
@@ -1,24 +1,23 @@
+import flow from "lodash/fp/flow";
+import map from "lodash/fp/map";
+import sortBy from "lodash/fp/sortBy";
+import filter from "lodash/fp/filter";
+import identity from "lodash/fp/identity";
+import isUndefined from "lodash/fp/isUndefined";
+
 import {
   getAverageOf,
   isValueInBetween,
   getNearestTo,
-} from "../src/utilities/utilities.js";
+} from "./utilities/utilities";
 
 import {
   fallbackFalseyFP,
   packIntoFP,
   getClosestFactorOfFP,
   crossFP,
-  getNearestDivisibleOfFP
-} from "./utilities/fp/utilities.js";
-
-const flow = require("lodash/fp/flow");
-const map = require("lodash/fp/map");
-const sortBy = require("lodash/fp/sortBy");
-const filter = require("lodash/fp/filter");
-const identity = require("lodash/fp/identity");
-const isUndefined = require("lodash/fp/isUndefined");
-
+  getNearestDivisibleOfFP,
+} from "./utilities/fp/utilities";
 
 export class SliderModel {
   constructor(options = {}) {
@@ -34,57 +33,54 @@ export class SliderModel {
     this.setOptions(options);
 
     Object.defineProperties(this._options, {
-      orientation: {writable: false},
-      hasTooltips: {writable: false},
+      orientation: { writable: false },
+      hasTooltips: { writable: false },
     });
   }
-
 
   get _boundaries() {
     return this._options.boundaries;
   }
-  set _boundaries(newValues) {
+
+  set _boundaries(values) {
     const currentValues = this._options.boundaries;
-    newValues = [].concat(newValues);
+    const newValues = [].concat(values);
 
     this._options.boundaries = flow(
-      sortBy( identity ),
-      map( parseFloat ),
-      filter( isFinite ),
-      crossFP( currentValues ),
+      sortBy(identity),
+      map(parseFloat),
+      filter(Number.isFinite),
+      crossFP(currentValues)
     )(newValues);
   }
-
 
   get _values() {
     return this._options.values;
   }
-  set _values(newValues) {
-    const step = this._options.step;
-    const [start, end] = this._options.boundaries;
-    const defaultValue = [
-      getAverageOf(this._options.boundaries),
-    ];
 
-    newValues = [].concat(newValues);
-    const currentValues = this._options.values &&
-      this._options.values.slice();
+  set _values(values) {
+    const { step } = this._options;
+    const [start, end] = this._options.boundaries;
+    const defaultValue = [getAverageOf(this._options.boundaries)];
+
+    const newValues = [].concat(values);
+    const currentValues = this._options.values && this._options.values.slice();
 
     this._options.values = flow(
-      sortBy( identity ),
-      map( parseFloat ),
-      filter( isFinite ),
-      crossFP( currentValues ),
-      fallbackFalseyFP( defaultValue ),
-      map( packIntoFP(start, end) ),
-      map( getNearestDivisibleOfFP(step, start) ),
+      sortBy(identity),
+      map(parseFloat),
+      filter(Number.isFinite),
+      crossFP(currentValues),
+      fallbackFalseyFP(defaultValue),
+      map(packIntoFP(start, end)),
+      map(getNearestDivisibleOfFP(step, start))
     )(newValues);
   }
-
 
   get _step() {
     return this._options.step;
   }
+
   set _step(newValue) {
     const [start, end] = this._options.boundaries;
     const range = end - start;
@@ -94,65 +90,62 @@ export class SliderModel {
       parseFloat,
       fallbackFalseyFP(currentValue),
       packIntoFP(1, range),
-      getClosestFactorOfFP(range),
+      getClosestFactorOfFP(range)
     )(newValue);
   }
-
 
   get _orientation() {
     return this._options.orientation;
   }
+
   set _orientation(value) {
     if (value !== "horizontal" && value !== "vertical") return;
 
     this._options.orientation = value;
   }
 
-
   get _hasTooltips() {
     return this._options.hasTooltips;
   }
+
   set _hasTooltips(value) {
     if (value !== false && value !== true) return;
 
     this._options.hasTooltips = value;
   }
 
-
-  setValueAt(index, newValue) {
+  setValueAt(index, value) {
     const currentValues = this._options.values;
     const newValues = currentValues.slice();
+    let newValue = value;
     const [prevValue, nextValue] = [
       currentValues[index - 1],
-      currentValues[index + 1]
+      currentValues[index + 1],
     ];
 
     if (currentValues.length === 1) {
-      this.setOptions( {values: newValue} );
+      this.setOptions({ values: newValue });
       return;
     }
 
-    if ( isUndefined(prevValue) ) {
-      newValue = ( newValue < nextValue ) ?
-        newValue : nextValue;
+    if (isUndefined(prevValue)) {
+      newValue = newValue < nextValue ? newValue : nextValue;
     }
 
-    if ( isUndefined(nextValue) ) {
-      newValue = ( newValue > prevValue ) ?
-        newValue : prevValue;
+    if (isUndefined(nextValue)) {
+      newValue = newValue > prevValue ? newValue : prevValue;
     }
 
-    if ( !isUndefined(prevValue) && !isUndefined(nextValue) ) {
-      newValue = isValueInBetween(newValue, prevValue, nextValue) ?
-        newValue :
-        getNearestTo(newValue, prevValue, nextValue);
+    if (!isUndefined(prevValue) && !isUndefined(nextValue)) {
+      newValue = isValueInBetween(newValue, prevValue, nextValue)
+        ? newValue
+        : getNearestTo(newValue, prevValue, nextValue);
     }
 
     newValues.splice(index, 1, newValue);
 
-    this.setOptions( {values: newValues} );
+    this.setOptions({ values: newValues });
   }
-
 
   getOptions() {
     return {
@@ -164,13 +157,11 @@ export class SliderModel {
     };
   }
 
-
   setOptions(newOptions) {
-    for ( let key of Object.keys(this._options) ) {
-      this["_" + key] = newOptions[key];
-    }
+    Object.keys(this._options).forEach((key) => {
+      this[`_${key}`] = newOptions[key];
+    });
 
     return this.getOptions();
   }
-
 }
