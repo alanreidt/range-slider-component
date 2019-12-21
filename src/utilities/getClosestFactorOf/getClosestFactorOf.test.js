11d773549fa4063e2882160b5e145b0ed40018a3
Alan Reid <alanreidt@gmail.com>
2019-12-21 23:37:57 +0300

    Update getClosestFactorOf test Correct according to eslint

--- src/utilities/getClosestFactorOf/getClosestFactorOf.test.js
+++ src/utilities/getClosestFactorOf/getClosestFactorOf.test.js
@@ -1,14 +1,13 @@
 import { getClosestFactorOf } from "./getClosestFactorOf";
 import { makeTestClass, template } from "../../../tests/testUtilities";
 
-
 export function testGetClosestFactorOf() {
-  describe("getClosestFactorOf", function () {
-    let describeTest = template`nearest divisor of division of ${0} by ${1} is ${"expectation"}`;
-    let TestClass = makeTestClass(getClosestFactorOf, describeTest);
+  describe("getClosestFactorOf", () => {
+    const describeTest = template`nearest divisor of division of ${0} by ${1} is ${"expectation"}`;
+    const TestClass = makeTestClass(getClosestFactorOf, describeTest);
 
-    describe("shall return nearest divisor", function () {
-      let funcOptions = [
+    describe("shall return nearest divisor", () => {
+      const funcOptions = [
         [900, 250],
         [120, 25],
         [100, 15],
@@ -19,28 +18,26 @@ export function testGetClosestFactorOf() {
         [25, 120],
         [500, 0],
       ];
-      let expectations = [225, 24, 20, 500, 1, 50, 100, 25, 1];
-      let test = new TestClass();
+      const expectations = [225, 24, 20, 500, 1, 50, 100, 25, 1];
+      const test = new TestClass();
       test.test(funcOptions, expectations);
     });
 
-
-    describe("shall return NaN, when operation can't be performed", function () {
-      let funcOptions = [
+    describe("shall return NaN, when operation can't be performed", () => {
+      const funcOptions = [
         [0, 375],
         [0, 0],
         [-120, 25],
         [-500, -375],
       ];
-      let expectations = new Array(funcOptions.length).fill(NaN);
-      let test = new TestClass();
+      const expectations = new Array(funcOptions.length).fill(NaN);
+      const test = new TestClass();
       test.test(funcOptions, expectations);
     });
 
-
-    context("shall catch garbage input", function () {
-      describe("returns NaN, if dividend parameter is incorrect", function () {
-        let funcOptions = [
+    context("shall catch garbage input", () => {
+      describe("returns NaN, if dividend parameter is incorrect", () => {
+        const funcOptions = [
           [undefined, 25],
           [null, 25],
           [Infinity, 25],
@@ -48,13 +45,13 @@ export function testGetClosestFactorOf() {
           ["text", 25],
           ["123text", 25],
         ];
-        let expectations = new Array(funcOptions.length).fill(NaN);
-        let test = new TestClass();
+        const expectations = new Array(funcOptions.length).fill(NaN);
+        const test = new TestClass();
         test.test(funcOptions, expectations);
       });
 
-      describe("returns NaN, if divisor parameter is incorrect", function () {
-        let funcOptions = [
+      describe("returns NaN, if divisor parameter is incorrect", () => {
+        const funcOptions = [
           [50, undefined],
           [50, null],
           [50, Infinity],
@@ -62,11 +59,10 @@ export function testGetClosestFactorOf() {
           [50, "text"],
           [50, "123text"],
         ];
-        let expectations = new Array(funcOptions.length).fill(NaN);
-        let test = new TestClass();
+        const expectations = new Array(funcOptions.length).fill(NaN);
+        const test = new TestClass();
         test.test(funcOptions, expectations);
       });
     });
-
   });
 }
