56460abc6048456fa72cdb66bbb29b4c47bb7e43
Alan Reid <alanreidt@gmail.com>
2019-12-21 22:57:59 +0300

    Update SliderAdapter Correct according to eslint

--- src/SliderAdapter.js
+++ src/SliderAdapter.js
@@ -1,16 +1,13 @@
 import { observerMixin } from "./utilities/observerMixin/observerMixin";
 
-
 export class SliderAdapter {
   constructor(dataSource) {
     this.dataSource = dataSource;
   }
 
-
   getOptions() {
     return this.dataSource.getOptions();
   }
-
 
   setOptions(options) {
     const newOptions = this.dataSource.setOptions(options);
@@ -18,13 +15,11 @@ export class SliderAdapter {
     this.triggerSubscribers("update", newOptions);
   }
 
-
   setValueAt(index, newValue) {
     this.dataSource.setValueAt(index, newValue);
 
-    this.triggerSubscribers("update", this.getOptions() );
+    this.triggerSubscribers("update", this.getOptions());
   }
-
 }
 
 Object.assign(SliderAdapter.prototype, observerMixin);
