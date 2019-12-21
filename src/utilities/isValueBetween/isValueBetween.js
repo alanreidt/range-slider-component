6c94a72469bf5bafcff6fed60743ffd9813a8b5c
Alan Reid <alanreidt@gmail.com>
2019-12-22 00:16:46 +0300

    Update isValueBetween Correct according to eslint

--- src/utilities/isValueBetween/isValueBetween.js
+++ src/utilities/isValueBetween/isValueBetween.js
@@ -4,13 +4,16 @@
  * @param {number} value The value, which is checked for attachment to interval.
  * @param {number} start The start of the interval.
  * @param {number} end The end of the interval.
+ *
  * @returns {boolean} True, if value is between start and end, false otherwise.
+ *
  */
 export function isValueBetween(value, start, end) {
-  if (Array.from(arguments).includes(null))
-    return false;
+  if (Array.from(arguments).includes(null)) return false;
+
   if (start > end) {
     [start, end] = [end, start];
   }
-  return (value > start) && (value < end);
+
+  return value > start && value < end;
 }
