372a003328dfbcfe709137e284950cc1cce7f345
Alan Reid <alanreidt@gmail.com>
2019-12-21 23:31:10 +0300

    Update getClosestFactorOf Correct according to eslint

--- src/utilities/getClosestFactorOf/getClosestFactorOf.js
+++ src/utilities/getClosestFactorOf/getClosestFactorOf.js
@@ -8,16 +8,15 @@
  * @returns {natural number} The closest factor of the dividend to the divisor.
  */
 export function getClosestFactorOf(dividend, divisor) {
-  if (!Number.isFinite(dividend) || !Number.isFinite(divisor) || (dividend <= 0))
+  if (!Number.isFinite(dividend) || !Number.isFinite(divisor) || dividend <= 0)
     return NaN;
-  if (divisor <= 0)
-    return 1;
-  for (let i = 0; i < dividend; i++) {
+  if (divisor <= 0) return 1;
+  for (let i = 0; i < dividend; i += 1) {
     if (dividend % (divisor + i) === 0) {
-      return (divisor + i);
+      return divisor + i;
     }
     if (dividend % (divisor - i) === 0) {
-      return (divisor - i);
+      return divisor - i;
     }
   }
   return dividend;
