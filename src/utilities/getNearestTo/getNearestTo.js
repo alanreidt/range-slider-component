ee1797c0a15086bc219b88d8d3e601113a341009
Alan Reid <alanreidt@gmail.com>
2019-12-22 00:00:09 +0300

    Update getNearestTo Correct according to eslint

--- src/utilities/getNearestTo/getNearestTo.js
+++ src/utilities/getNearestTo/getNearestTo.js
@@ -4,19 +4,23 @@
  *
  * @param {number} value The value, which nearest number is compared to.
  * @param {number} args The number, that is compared to the value.
+ *
  * @returns {number} The nearest number to the value.
  * @returns {NaN} Error.
  */
 export function getNearestTo(value, ...args) {
-  if (!Number.isFinite(value))
-    return NaN;
-  let filteredArray = args.filter(item => Number.isFinite(item));
-  if (!filteredArray.length)
-    return NaN;
-  let result = filteredArray.reduce((prev, current) => {
-    let currentDifference = Math.abs(value - current);
-    let prevDifference = Math.abs(value - prev);
-    return (currentDifference <= prevDifference) ? current : prev;
+  if (!Number.isFinite(value)) return NaN;
+
+  const filteredArray = args.filter((item) => Number.isFinite(item));
+
+  if (!filteredArray.length) return NaN;
+
+  const result = filteredArray.reduce((prev, current) => {
+    const currentDifference = Math.abs(value - current);
+    const prevDifference = Math.abs(value - prev);
+
+    return currentDifference <= prevDifference ? current : prev;
   });
+
   return Number.isFinite(result) ? result : NaN;
 }
