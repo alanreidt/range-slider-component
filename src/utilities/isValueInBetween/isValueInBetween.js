14530720ef445dee8408b0f13c001b2b5a0adb7d
Alan Reid <alanreidt@gmail.com>
2019-12-22 00:26:27 +0300

    Update isValueInBetween Change logic: wrap isValueBetween and correct arguments

--- src/utilities/isValueInBetween/isValueInBetween.js
+++ src/utilities/isValueInBetween/isValueInBetween.js
@@ -1,16 +1,15 @@
+import { isValueBetween } from "../isValueBetween/isValueBetween";
+
 /**
  * Defines whether value is between start and end, including the edges, or not.
  *
  * @param {number} value The value, which is checked for attachment to interval.
  * @param {number} start The start of the interval.
  * @param {number} end The end of the interval.
+ *
  * @returns {boolean} True, if value is between start and end, including the edges, false otherwise.
+ *
  */
 export function isValueInBetween(value, start, end) {
-  if ([].includes.call(arguments, null))
-    return false;
-  if (start > end) {
-    [start, end] = [end, start];
-  }
-  return (value >= start) && (value <= end);
+  return isValueBetween(value, start - 1, end - 1);
 }
