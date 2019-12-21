0cb0cedcd73d90906e4c7d2430c6aa713e252949
Alan Reid <alanreidt@gmail.com>
2019-12-22 00:02:01 +0300

    Update getNearestTo test Correct according to eslint

--- src/utilities/getNearestTo/getNearestTo.test.js
+++ src/utilities/getNearestTo/getNearestTo.test.js
@@ -2,12 +2,12 @@ import { getNearestTo } from "./getNearestTo";
 import { test, template } from "../../../tests/testUtilities";
 
 export function testGetNearestTo() {
-  describe("getNearestTo", function () {
-    let describeTest = template`nearest number to ${0} from ${"...rest"} is ${"expectation"}`;
-    let testCurrent = test(getNearestTo, describeTest);
+  describe("getNearestTo", () => {
+    const describeTest = template`nearest number to ${0} from ${"...rest"} is ${"expectation"}`;
+    const testCurrent = test(getNearestTo, describeTest);
 
-    describe("shall return nearest number to value", function () {
-      let funcOptions = [
+    describe("shall return nearest number to value", () => {
+      const funcOptions = [
         [100, 50, 90],
         [-100, -50, -90],
         [0, -50, 90],
@@ -15,34 +15,31 @@ export function testGetNearestTo() {
         [250, -100, 100, 250],
         [100, 50, 90, 105],
       ];
-      let expectations = [90, -90, -50, 100, 250, 105];
+      const expectations = [90, -90, -50, 100, 250, 105];
       testCurrent(funcOptions, expectations);
     });
 
-
-    describe("shall return last nearest number to value, if controvertial", function () {
-      let funcOptions = [
+    describe("shall return last nearest number to value, if controversial", () => {
+      const funcOptions = [
         [100, 99, 105, 101, 5],
         [100, 101, 105, 99, 5],
       ];
-      let expectations = [101, 99];
+      const expectations = [101, 99];
       testCurrent(funcOptions, expectations);
     });
 
-
-    describe("shall filter incorrect arguments", function () {
-      let funcOptions = [
+    describe("shall filter incorrect arguments", () => {
+      const funcOptions = [
         [100, null, 105, undefined, 5],
         [0, null, 105, Infinity, 5],
       ];
-      let expectations = [105, 5];
+      const expectations = [105, 5];
       testCurrent(funcOptions, expectations);
     });
 
-
-    context("shall catch garbage input", function () {
-      describe("returns NaN if value parameter is incorrect", function () {
-        let funcOptions = [
+    context("shall catch garbage input", () => {
+      describe("returns NaN if value parameter is incorrect", () => {
+        const funcOptions = [
           [undefined, -100, 100],
           [null, -100, 100],
           [Infinity, -100, 100],
@@ -50,12 +47,12 @@ export function testGetNearestTo() {
           ["text", -100, 100],
           ["123text", -100, 100],
         ];
-        let expectations = new Array(funcOptions.length).fill(NaN);
+        const expectations = new Array(funcOptions.length).fill(NaN);
         testCurrent(funcOptions, expectations);
       });
 
-      describe("returns NaN if args parameter is incorrect", function () {
-        let funcOptions = [
+      describe("returns NaN if args parameter is incorrect", () => {
+        const funcOptions = [
           [50, undefined],
           [50, null],
           [50, Infinity],
@@ -63,10 +60,9 @@ export function testGetNearestTo() {
           [50, "text"],
           [50, "123text"],
         ];
-        let expectations = new Array(funcOptions.length).fill(NaN);
+        const expectations = new Array(funcOptions.length).fill(NaN);
         testCurrent(funcOptions, expectations);
       });
     });
-
   });
 }
