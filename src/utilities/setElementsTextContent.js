494b3b9b5099471256f30645b3ebfc14f2d8d7bb
Alan Reid <alanreidt@gmail.com>
2019-12-22 13:07:22 +0300

    Update setElementsTextContent Correct according to eslint

--- src/utilities/setElementsTextContent.js
+++ src/utilities/setElementsTextContent.js
@@ -1,7 +1,6 @@
 import { call } from "./call/call";
 import { setElementTextContent } from "./setElementTextContent/setElementTextContent";
 
-
 export function setElementsTextContent(elements, values) {
-  elements.forEach( call(setElementTextContent, values) );
+  elements.forEach(call(setElementTextContent, values));
 }
