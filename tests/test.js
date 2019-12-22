0f05dd930dfc40d19e38ca0c33ced74a746748c9
Alan Reid <alanreidt@gmail.com>
2019-12-22 14:02:28 +0300

    Update test.js Correct according to eslint

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,15 @@
 import { testGetClosestFactorOf } from "../src/utilities/getClosestFactorOf/getClosestFactorOf.test";
-import { testGetNearestTo } from "../src/utilities/getNearestTo/getNearestTo.test.js";
-import { testIsValueBetween } from "../src/utilities/isValueBetween/isValueBetween.test.js";
-import { testGetOverstepOf } from "../src/utilities/getOverstepOf/getOverstepOf.test.js";
-import { testGetNearestDivisibleOf } from "../src/utilities/getNearestDivisibleOf/getNearestDivisibleOf.test.js";
-import { testGetPositionInPercentageOf } from "../src/utilities/getPositionInPercentageOf/getPositionInPercentageOf.test.js";
-import { testTranslateProportionIntoValue } from "../src/utilities/translateProportionIntoValue/translateProportionIntoValue.test.js";
-import { testObserverMixin } from "../src/utilities/observerMixin/observerMixin.test.js";
-import { testSliderAdapter } from "./SliderAdapter.test.js";
-import { testSliderModel } from "./SliderModel.test.js";
-import { testSlider } from "./slider.test.js";
-import { testSetElementPosition } from "../src/utilities/setElementPosition/setElementPosition.test.js";
+import { testGetNearestTo } from "../src/utilities/getNearestTo/getNearestTo.test";
+import { testIsValueBetween } from "../src/utilities/isValueBetween/isValueBetween.test";
+import { testGetOverstepOf } from "../src/utilities/getOverstepOf/getOverstepOf.test";
+import { testGetNearestDivisibleOf } from "../src/utilities/getNearestDivisibleOf/getNearestDivisibleOf.test";
+import { testGetPositionInPercentageOf } from "../src/utilities/getPositionInPercentageOf/getPositionInPercentageOf.test";
+import { testTranslateProportionIntoValue } from "../src/utilities/translateProportionIntoValue/translateProportionIntoValue.test";
+import { testObserverMixin } from "../src/utilities/observerMixin/observerMixin.test";
+import { testSliderAdapter } from "./SliderAdapter.test";
+import { testSliderModel } from "./SliderModel.test";
+import { testSlider } from "./slider.test";
+import { testSetElementPosition } from "../src/utilities/setElementPosition/setElementPosition.test";
 import { testSliderUI } from "./SliderUI.test";
 
 testGetClosestFactorOf();
