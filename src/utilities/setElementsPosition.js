8a6947d6cee492f0e8d2d87974d88971a5d403c4
Alan Reid <alanreidt@gmail.com>
2019-12-22 13:06:55 +0300

    Update setElementsPosition Correct according to eslint

--- src/utilities/setElementsPosition.js
+++ src/utilities/setElementsPosition.js
@@ -1,7 +1,6 @@
 import { call } from "./call/call";
 import { setElementPosition } from "./setElementPosition/setElementPosition";
 
-
 export function setElementsPosition(elements, positions, property) {
   elements.forEach(call(setElementPosition, positions, property));
 }
