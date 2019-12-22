aae04d3803bcf498d2f13d413ae6c0b4339ecc9f
Alan Reid <alanreidt@gmail.com>
2019-12-22 12:30:58 +0300

    Update packInto Correct according to eslint

--- src/utilities/packInto/packInto.js
+++ src/utilities/packInto/packInto.js
@@ -2,9 +2,7 @@ import { isValueInBetween } from "../isValueInBetween/isValueInBetween";
 import { getNearestTo } from "../getNearestTo/getNearestTo";
 
 export function packInto(value, start, end) {
-
-  return isValueInBetween(value, start, end) ?
-    value :
-    getNearestTo(value, start, end);
-
+  return isValueInBetween(value, start, end)
+    ? value
+    : getNearestTo(value, start, end);
 }
