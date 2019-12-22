13197dcf4c31ce1fcff5592f81b47553d237f74b
Alan Reid <alanreidt@gmail.com>
2019-12-22 12:25:27 +0300

    Update observerMixin test Correct according to eslint

--- src/utilities/observerMixin/observerMixin.test.js
+++ src/utilities/observerMixin/observerMixin.test.js
@@ -1,104 +1,101 @@
+import { assert } from "chai";
+
 import { observerMixin } from "./observerMixin";
 
-
 export function testObserverMixin() {
-
-
-  describe("observerMixin", function () {
-
-    describe("shall store list of subscribers", function () {
-      let publisher = Object.assign({}, observerMixin);
-      let subscriber1 = {};
-      let subscriber2 = {};
-      let subscribers = [subscriber1, subscriber2];
+  describe("observerMixin", () => {
+    describe("shall store list of subscribers", () => {
+      const publisher = { ...observerMixin };
+      const subscriber1 = {};
+      const subscriber2 = {};
+      const subscribers = [subscriber1, subscriber2];
 
       publisher.addSubscriber("change", subscriber1);
       publisher.addSubscriber("change", subscriber2);
 
       subscribers.forEach((subscriber, i) => {
-        let publisherSubscriber = publisher._eventSubscribers.change[i];
+        const publisherSubscriber = publisher._eventSubscribers.change[i];
 
-        it(`subscriber${i + 1} is in the list`, function () {
+        it(`subscriber${i + 1} is in the list`, () => {
           assert.equal(subscriber, publisherSubscriber);
         });
       });
     });
 
-
-    describe("shall remove subscribers from the list by request", function () {
-      let publisher = Object.assign({}, observerMixin);
-      let subscriber1 = {};
-      let subscriber2 = {};
-      let subscriber3 = {};
-      let subscribers = [subscriber1, subscriber2, subscriber3];
-      let cuttedSubscribers = [subscriber1, subscriber3];
+    describe("shall remove subscribers from the list by request", () => {
+      const publisher = { ...observerMixin };
+      const subscriber1 = {};
+      const subscriber2 = {};
+      const subscriber3 = {};
+      const subscribers = [subscriber1, subscriber2, subscriber3];
+      const cuttedSubscribers = [subscriber1, subscriber3];
 
       publisher.addSubscriber("change", subscriber1);
       publisher.addSubscriber("change", subscriber2);
       publisher.addSubscriber("change", subscriber3);
 
-      let publisherSubscribers = publisher._eventSubscribers.change;
+      const publisherSubscribers = publisher._eventSubscribers.change;
 
       subscribers.forEach((subscriber, i) => {
-        let publisherSubscriber = publisher._eventSubscribers.change[i];
+        const publisherSubscriber = publisher._eventSubscribers.change[i];
 
-        it(`subscriber${i + 1} is in the list`, function () {
+        it(`subscriber${i + 1} is in the list`, () => {
           assert.equal(subscriber, publisherSubscriber);
         });
       });
 
       publisher.removeSubscriber("change", subscriber2);
 
-      it(`subscriber2 is removed from the list`, function () {
+      it(`subscriber2 is removed from the list`, () => {
         assert.deepEqual(cuttedSubscribers, publisherSubscribers);
       });
     });
 
-
-    describe("shall trigger subscribers", function () {
-      const publisher = Object.assign({}, observerMixin);
+    describe("shall trigger subscribers", () => {
+      const publisher = { ...observerMixin };
       const subscribers = [];
 
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < 3; i += 1) {
         subscribers.push({
           isNotified: false,
           update(value) {
             this.isNotified = true;
             this.value = value;
-          }
+          },
         });
       }
 
-      const subscriberHandlers = subscribers.map((subscriber) => subscriber.update.bind(subscriber));
+      const subscriberHandlers = subscribers.map((subscriber) =>
+        subscriber.update.bind(subscriber),
+      );
 
-      subscriberHandlers.forEach((subscriberHandler) => publisher.addSubscriber("change", subscriberHandler));
+      subscriberHandlers.forEach((subscriberHandler) =>
+        publisher.addSubscriber("change", subscriberHandler),
+      );
 
       const publisherSubscribers = publisher._eventSubscribers.change;
 
-      it(`subscribers are in the list`, function () {
+      it(`subscribers are in the list`, () => {
         assert.deepEqual(subscriberHandlers, publisherSubscribers);
       });
 
       publisher.triggerSubscribers("change", "Hello");
 
-      context(`subscribers are notified`, function () {
+      context(`subscribers are notified`, () => {
         subscribers.forEach((subscriber, i) => {
-          it(`subscriber${i + 1} is notified`, function () {
+          it(`subscriber${i + 1} is notified`, () => {
             assert.isTrue(subscriber.isNotified);
           });
         });
       });
 
-      context(`value is accepted`, function () {
+      context(`value is accepted`, () => {
         subscribers.forEach((subscriber, i) => {
-          it(`subscriber${i + 1} has value`, function () {
+          it(`subscriber${i + 1} has value`, () => {
             assert.equal(subscriber.value, "Hello");
           });
         });
       });
     });
-
   });
-
-
 }
