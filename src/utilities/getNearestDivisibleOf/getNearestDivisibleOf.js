324587d591f79ca12fa6bf90b3efa95d8abab724
Alan Reid <alanreidt@gmail.com>
2019-12-23 22:57:31 +0300

    Update getNearestDivisibleOf function Remove redundant start parameter

--- src/utilities/getNearestDivisibleOf/getNearestDivisibleOf.js
+++ src/utilities/getNearestDivisibleOf/getNearestDivisibleOf.js
@@ -1,20 +1,17 @@
 /**
- * If this can be done, returns the nearest number (the bigger one, if controversial)
+ * If this can be done, returns the closest number (the bigger one, if controversial)
  * that can be divided by divisor without the remainder.
  * Otherwise, returns NaN.
  *
  * @param {number} dividend The number, that to be divided.
  * @param {number} divisor The number, that divides the dividend.
- * @param {number} start The number which to start to counting from.
- * @returns {number} The nearest number, that can be divided by divisor without the remainder.
+ *
+ * @returns {number} The closest number, that can be divided by divisor without the remainder.
  */
-export function getNearestDivisibleOf(dividend, divisor, start) {
-  start = start || 0;
+export function getNearestDivisibleOf(dividend, divisor) {
   const absoluteOfDivisor = Math.abs(divisor);
-  dividend -= start;
 
-  const result =
-    Math.round(dividend / absoluteOfDivisor) * absoluteOfDivisor + start;
+  const result = Math.round(dividend / absoluteOfDivisor) * absoluteOfDivisor;
 
   return Number.isFinite(result) ? result : NaN;
 }
