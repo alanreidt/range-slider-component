ff110fbcea46b86122526f750f250b1b92154ace
Alan Reid <alanreidt@gmail.com>
2019-12-29 17:56:30 +0300

    Correct isDivisible function

--- src/utilities/isDivisible/isDivisible.js
+++ src/utilities/isDivisible/isDivisible.js
@@ -3,11 +3,7 @@
  *
  * @param {number} dividend The number, that to be divided.
  * @param {number} divisor The number, that divides the dividend.
- * @param {number} start The number which to start to counting from.
- *
- * @returns {boolean} True, if dividend can be divided by divisor without the remainder. Otherwise, false.
- *
  */
-export function isDivisible(dividend, divisor, start = 0) {
-  return (dividend - start) % divisor === 0;
+export function isDivisible(dividend, divisor) {
+  return dividend % divisor === 0;
 }
