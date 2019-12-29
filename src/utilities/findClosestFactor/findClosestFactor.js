9816a7299dfc37d052b1ce35fa915a25f67a87c4
Alan Reid <alanreidt@gmail.com>
2019-12-29 17:33:03 +0300

    Refactor findClosestFactor function

--- src/utilities/findClosestFactor/findClosestFactor.js
+++ src/utilities/findClosestFactor/findClosestFactor.js
@@ -1,3 +1,5 @@
+import { isDivisible } from "../isDivisible/isDivisible";
+
 /**
  * If this can be done, returns the closest factor (natural number (positive integer))
  * of division (the bigger one, if controversial).
@@ -9,24 +11,32 @@
  * @returns {number} The closest factor (natural number (positive integer)) of the division.
  */
 export function findClosestFactor(dividend, divisor) {
-  if (
-    !Number.isFinite(dividend) ||
-    !Number.isFinite(divisor) ||
-    dividend <= 0
-  ) {
+  const isArgumentsCorrect =
+    Number.isFinite(dividend) && Number.isFinite(divisor) && dividend > 0;
+
+  if (!isArgumentsCorrect) {
     return NaN;
   }
 
-  if (divisor <= 0) return 1;
+  if (divisor <= 0) {
+    return 1;
+  }
 
-  for (let i = 0; i < dividend; i += 1) {
-    if (dividend % (divisor + i) === 0) {
-      return divisor + i;
+  let nextDivisor = divisor;
+  let prevDivisor = divisor;
+  const isExtremum = nextDivisor === dividend || prevDivisor === 1;
+
+  while (!isExtremum) {
+    if (isDivisible(dividend, nextDivisor)) {
+      return nextDivisor;
     }
 
-    if (dividend % (divisor - i) === 0) {
-      return divisor - i;
+    if (isDivisible(dividend, prevDivisor)) {
+      return prevDivisor;
     }
+
+    nextDivisor += 1;
+    prevDivisor -= 1;
   }
 
   return dividend;
