21ae14d6a6c2622a6e7be3e997c7f7217b7afead
Alan Reid <alanreidt@gmail.com>
2019-12-29 12:35:21 +0300

    Update adjustValueToStep function Add JS Doc description

--- src/helpers/adjustValueToStep/adjustValueToStep.js
+++ src/helpers/adjustValueToStep/adjustValueToStep.js
@@ -8,4 +8,14 @@ function base(value, step, offset) {
   return closestDivisible + offset;
 }
 
+/**
+ * Adjust a value, in order to be divisible by a step.
+ * Returns NaN, if operation can't be performed.
+ *
+ * @param {number} value The value, that to be adjusted.
+ * @param {number} step The step, that divides the value.
+ * @param {number} offset The offset of a starting point for the step, counting from 0.
+ *
+ * @returns {number} The adjusted value, that can be divided by the step without remainder.
+ */
 export const adjustValueToStep = curryRight(base);
