4e7769763d3bf6b13ef0a85f69c7e8537f9fd84b
Alan Reid <alanreidt@gmail.com>
2020-01-02 13:12:09 +0300

    Move findValuePositionBetween from utilities to helpers

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -13,7 +13,6 @@ export { findClosestDivisible } from "./findClosestDivisible/findClosestDivisibl
 export { getNearestTo } from "./getNearestTo/getNearestTo";
 export { isValueBetween } from "./isValueBetween/isValueBetween";
 export { translateProportionIntoValue } from "./translateProportionIntoValue/translateProportionIntoValue";
-export { findValuePositionBetween } from "./findValuePositionBetween/findValuePositionBetween";
 export { findClosestFactor } from "./findClosestFactor/findClosestFactor";
 export { getAverageOf } from "./getAverageOf/getAverageOf";
 export { getNextDivisibleOf } from "./getNextDivisibleOf/getNextDivisibleOf";
