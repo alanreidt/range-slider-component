00a6213ac8ad82936dba4983dc2f23c8e4256101
Alan Reid <alanreidt@gmail.com>
2020-01-06 13:04:00 +0300

    Add description to the findValueByRatioBetween function

--- src/helpers/findValueByRatioBetween/findValueByRatioBetween.js
+++ src/helpers/findValueByRatioBetween/findValueByRatioBetween.js
@@ -1,5 +1,15 @@
 import { findAntecedent } from "../../utilities/findAntecedent";
 
+/**
+ * Returns value between start and end, via known ratio, if it's possible.
+ * Otherwise, returns NaN.
+ *
+ * @param {number} ratio A ratio between desired value and range (a difference between start and end).
+ * @param {number} start A start of the range, where value to be found.
+ * @param {number} end An end of the range, where value to be found.
+ *
+ * @returns {number} A value between the start and the end.
+ */
 export function findValueByRatioBetween(ratio, start, end) {
   const areArgumentsCorrect = end !== null;
 
