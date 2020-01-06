25c4ce52014ed28b3ed1bdc60da0c073c69416f8
Alan Reid <alanreidt@gmail.com>
2020-01-06 13:10:55 +0300

    Update description of findValuePositionBetween function

--- src/helpers/findValuePositionBetween/findValuePositionBetween.js
+++ src/helpers/findValuePositionBetween/findValuePositionBetween.js
@@ -1,14 +1,14 @@
 import { findRatio, toPercentage } from "../../utilities";
 
 /**
- * Returns position of a value between start and end, if it's possible.
- * Otherwise, returns NaN
+ * Returns a position of a value between start and end, if it's possible.
+ * Otherwise, returns NaN.
  *
  * @param {number} value The value, which position to be found.
- * @param {number} start The start of the range, where position to be found.
- * @param {number} end The end of the range, where position to be found.
+ * @param {number} start The start of a range, where position to be found.
+ * @param {number} end The end of a range, where position to be found.
  *
- * @returns {number} The position of the value between start and end.
+ * @returns {number} The position of the value between the start and the end.
  */
 export function findValuePositionBetween(value, start, end) {
   const areArgumentsCorrect = Array.from(arguments).every(Number.isFinite);
