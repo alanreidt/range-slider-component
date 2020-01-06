b439be9a4291b1ea29211d39dbd088630caae137
Alan Reid <alanreidt@gmail.com>
2020-01-06 12:43:33 +0300

    Update findValueByRatioBetween function Correct parameters filtration logic: allow Infinity

--- src/helpers/findValueByRatioBetween/findValueByRatioBetween.js
+++ src/helpers/findValueByRatioBetween/findValueByRatioBetween.js
@@ -1,7 +1,7 @@
 import { findAntecedent } from "../../utilities/findAntecedent";
 
 export function findValueByRatioBetween(ratio, start, end) {
-  const areArgumentsCorrect = Array.from(arguments).every(Number.isFinite);
+  const areArgumentsCorrect = end !== null;
 
   if (!areArgumentsCorrect) {
     return NaN;
