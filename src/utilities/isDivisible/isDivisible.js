f3cef02ce088fbcd49577ab6f6e05923807afa51
Alan Reid <alanreidt@gmail.com>
2020-01-11 14:28:39 +0300

    Correct isDivisible function description

--- src/utilities/isDivisible/isDivisible.js
+++ src/utilities/isDivisible/isDivisible.js
@@ -1,8 +1,8 @@
 /**
  * Defines whether dividend can be divided by divisor without the remainder or not.
  *
- * @param {number} dividend The number, that to be divided.
- * @param {number} divisor The number, that divides the dividend.
+ * @param {number} dividend A number, that to be divided.
+ * @param {number} divisor A number, that divides the dividend.
  */
 export function isDivisible(dividend, divisor) {
   return dividend % divisor === 0;
