1a9653d725b6cecb7e57fcb5edff44ea03b289d4
Alan Reid <alanreidt@gmail.com>
2020-01-23 22:08:45 +0300

    Rename placeValueBetween function to placeNumberBetween

--- src/utilities/placeValueBetween/placeValueBetween.js
+++
@@ -1,8 +0,0 @@
-import { isNumberInBetween } from "../isNumberInBetween/isNumberInBetween";
-import { findClosestTo } from "../findClosestTo/findClosestTo";
-
-export function placeValueBetween(value, start, end) {
-  return isNumberInBetween(value, start, end)
-    ? value
-    : findClosestTo(value, start, end);
-}
