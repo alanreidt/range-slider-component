1a92a1eabd15af948df2b9e3a28a0cc1edb1eae3
Alan Reid <alanreidt@gmail.com>
2020-01-29 20:14:51 +0300

    Move testClass function into testUtilities folder

---
+++ tests/testUtilities/index.js
@@ -0,0 +1,1 @@
+export { testClass } from "./testClass";
---
+++ tests/testUtilities/testClass.js
@@ -0,0 +1,49 @@
+import assert from "assert";
+
+export function testClass({ Class, methodName, methodGetterName } = {}) {
+  return function({ constructorArgs, methodArgsList, expectations } = {}) {
+    let constructorArgsRecord = "";
+
+    for (const constructorArgsKey in constructorArgs) {
+      const constructorArgsValue = constructorArgs[constructorArgsKey];
+
+      constructorArgsRecord += `${constructorArgsKey} = ${constructorArgsValue}, `;
+    }
+
+    expectations.forEach((expectation, index) => {
+      const subject = new Class(constructorArgs);
+      const methodArgs = methodArgsList && methodArgsList[index];
+      let methodArgsRecord = "";
+
+      if (methodName && methodArgs) {
+        for (const methodArgsKey in methodArgs) {
+          const methodArgsValue = methodArgs[methodArgsKey];
+
+          methodArgsRecord += `${methodArgsKey} = ${methodArgsValue}, `;
+        }
+
+        subject[methodName](methodArgs);
+      }
+
+      if (methodName && !methodArgs) {
+        methodArgsRecord = constructorArgsRecord;
+
+        subject[methodName]();
+      }
+
+      const subjectOutput = subject[methodGetterName]();
+      methodArgsRecord = methodArgsRecord || "default values";
+
+      context(`if ${methodArgsRecord} were passed`, function() {
+        for (const expectationKey in expectation) {
+          const expectationValue = expectation[expectationKey];
+          const subjectOutputValue = subjectOutput[expectationKey];
+
+          it(`than ${expectationKey} shall be equal to ${expectationValue}`, function() {
+            assert.deepEqual(subjectOutputValue, expectationValue);
+          });
+        }
+      });
+    });
+  };
+}
