eccbfabc80982449be013221dbf39320dab3581b
Alan Reid <alanreidt@gmail.com>
2020-01-30 21:02:10 +0300

    Add descrition to the makeTestClass function

--- tests/testUtilities/makeTestClass.js
+++ tests/testUtilities/makeTestClass.js
@@ -2,17 +2,35 @@ import { assert } from "chai";
 
 import { testDescriptionTemplate } from "./testDescriptionTemplate";
 
+/**
+ * Returns a class for a test.
+ *
+ * @param {Object} subject A subject of the test.
+ * @param {testDescriptionTemplate} commonDescriptionTemplate a test description template,
+ *  composed via testDescriptionTemplate testUtilities function.
+ */
 export function makeTestClass(
   subject,
   commonDescriptionTemplate = testDescriptionTemplate`${"...rest"} is equal to ${"expectation"}`,
 ) {
   return class {
+    /**
+     * @param {testDescriptionTemplate} localDescriptionTemplate a test description template,
+     *  composed via testDescriptionTemplate testUtilities function.
+     *  Will replace commonDescriptionTemplate, if it's passed.
+     */
     constructor(localDescriptionTemplate) {
       this.subject = subject;
       this.composeDescription =
         localDescriptionTemplate || commonDescriptionTemplate;
     }
 
+    /**
+     * Run the test.
+     *
+     * @param {any} funcArgsList testing function arguments array
+     * @param {any} expectations test expectations
+     */
     run(funcArgsList, expectations) {
       expectations.forEach((expectation, index) => {
         const funcArgs = funcArgsList[index];
