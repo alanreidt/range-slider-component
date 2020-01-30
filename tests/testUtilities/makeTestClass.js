9a6f631e2e91a76d75f34069161bbec669b59c48
Alan Reid <alanreidt@gmail.com>
2020-01-30 20:44:41 +0300

    Rename makeTestClass function parameters

--- tests/testUtilities/makeTestClass.js
+++ tests/testUtilities/makeTestClass.js
@@ -4,19 +4,20 @@ import { testDescriptionTemplate } from "./testDescriptionTemplate";
 
 export function makeTestClass(
   subject,
-  commonDescription = testDescriptionTemplate`${"...rest"} is equal to ${"expectation"}`,
+  commonDescriptionTemplate = testDescriptionTemplate`${"...rest"} is equal to ${"expectation"}`,
 ) {
   return class {
-    constructor(localDescription) {
+    constructor(localDescriptionTemplate) {
       this.subject = subject;
-      this.description = localDescription || commonDescription;
+      this.composeDescription =
+        localDescriptionTemplate || commonDescriptionTemplate;
     }
 
     run(funcArgsList, expectations) {
       expectations.forEach((expectation, index) => {
         const funcArgs = funcArgsList[index];
 
-        it(this.description(funcArgs, expectation), () => {
+        it(this.composeDescription(funcArgs, expectation), () => {
           assert.deepEqual(this.subject(...funcArgs), expectation);
         });
       });
