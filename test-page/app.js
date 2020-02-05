1b6dded600207ebe9773ee8317d2fb490579fa51
Alan Reid <alanreidt@gmail.com>
2020-02-05 22:27:33 +0300

    Correct demo-page app.js file

--- test-page/app.js
+++ test-page/app.js
@@ -1,29 +1,24 @@
 import { slider } from "../src/slider";
 
-const sliderWrappers = document.querySelectorAll(".js-slider-wrapper");
-const sliderWrapper0 = sliderWrappers[0];
-const sliderWrapper1 = sliderWrappers[1];
-const sliderWrapper2 = sliderWrappers[2];
+const parents = document.querySelectorAll(".js-slider-wrapper");
+const parent0 = parents[0];
+const parent1 = parents[1];
+const parent2 = parents[2];
 
-slider.create(sliderWrapper0, {
+slider.create(parent0, {
   boundaries: [-30000, 30000],
   values: [-20000, 10000, 20000],
   step: 1000,
   hasTooltips: true,
 });
 
-slider.create(sliderWrapper1, {
+slider.create(parent1, {
   values: [20, 80],
   step: 1,
-  // orientation: "vertical",
   hasTooltips: true,
 });
 
-// slider.setOptions(sliderWrapper1, {
-//   boundaries: [-500, 300],
-// });
-
-slider.create(sliderWrapper2, {
+slider.create(parent2, {
   orientation: "vertical",
   hasTooltips: true,
 });
