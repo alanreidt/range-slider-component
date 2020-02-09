203add2544e00a0e31af433711eff59ac03ace93
Alan Reid <alanreidt@gmail.com>
2020-02-09 11:39:31 +0300

    Update Model Refactor the setValueAt method

--- src/Model.js
+++ src/Model.js
@@ -51,12 +51,6 @@ export class Model {
   }
 
   setValueAt(index, value) {
-    const areArgumentsCorrect = Array.from(arguments).every(Number.isFinite);
-
-    if (!areArgumentsCorrect) {
-      return;
-    }
-
     const currentValues = this._options.values;
     const [prevValue, nextValue] = [
       currentValues[index - 1],
@@ -64,8 +58,10 @@ export class Model {
     ];
 
     const values = flow(
+      parseFloat,
       restrictByNeighbors(prevValue, nextValue),
       replaceAt(index, currentValues),
+      filter(Number.isFinite),
     )(value);
 
     this.setOptions({ values });
