dc5b3db7fd1be168c21b22992668e9c0fe8e8697
Alan Reid <alanreidt@gmail.com>
2020-02-12 12:44:35 +0300

    Update cross function test descriptions

--- src/utilities/cross/cross.test.js
+++ src/utilities/cross/cross.test.js
@@ -5,21 +5,21 @@ import { cross } from "./cross";
 describe("cross(baseArr, arr)", async (assert) => {
   assert({
     given: "baseArr with identical items",
-    should: "return sorted array",
+    should: "return crossed sorted array",
     actual: cross([40, 40, 40, 40], [20]),
     expected: [20, 40, 40, 40],
   });
 
   assert({
     given: "baseArr with identical items",
-    should: "return sorted array",
+    should: "return crossed sorted array",
     actual: cross([40, 40, 40, 40], [40]),
     expected: [40, 40, 40, 40],
   });
 
   assert({
     given: "baseArr with identical items",
-    should: "return sorted array",
+    should: "return crossed sorted array",
     actual: cross([40, 40, 40, 40], [60]),
     expected: [40, 40, 40, 60],
   });
