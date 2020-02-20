c2710fbcc2ad259a08a8eaccffb69eb14aa18623
Alan Reid <alanreidt@gmail.com>
2020-02-20 13:14:59 +0300

    Update helpers index.js Replace double quotes with single ones

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,11 @@
 /* PLOP_INJECT_EXPORT */
-export { replaceAt } from "./replaceAt";
-export { restrictByNeighbors } from "./restrictByNeighbors";
-export { crossWith } from "./crossWith";
-export { eitherOr } from "./eitherOr";
-export { placeBetween } from "./placeBetween";
-export { adjustToRange } from "./adjustToRange";
-export { createTemplate } from "./createTemplate";
-export { findValueByRatioBetween } from "./findValueByRatioBetween";
-export { findValuePositionBetween } from "./findValuePositionBetween";
-export { adjustValueToStep } from "./adjustValueToStep";
+export { replaceAt } from './replaceAt';
+export { restrictByNeighbors } from './restrictByNeighbors';
+export { crossWith } from './crossWith';
+export { eitherOr } from './eitherOr';
+export { placeBetween } from './placeBetween';
+export { adjustToRange } from './adjustToRange';
+export { createTemplate } from './createTemplate';
+export { findValueByRatioBetween } from './findValueByRatioBetween';
+export { findValuePositionBetween } from './findValuePositionBetween';
+export { adjustValueToStep } from './adjustValueToStep';
