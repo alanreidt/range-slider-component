90dcfd83816aaaae53944c5187f608311374b698
Alan Reid <alanreidt@gmail.com>
2020-02-20 18:15:28 +0300

    Update cross function test Replace double quotes with single one

--- src/utilities/cross/cross.test.js
+++ src/utilities/cross/cross.test.js
@@ -1,25 +1,25 @@
-import { describe } from "riteway";
+import { describe } from 'riteway';
 
-import { cross } from "./cross";
+import { cross } from './cross';
 
-describe("cross(baseArr, arr)", async (assert) => {
+describe('cross(baseArr, arr)', async (assert) => {
   assert({
-    given: "baseArr with identical items",
-    should: "return crossed sorted array",
+    given: 'baseArr with identical items',
+    should: 'return crossed sorted array',
     actual: cross([40, 40, 40, 40], [20]),
     expected: [20, 40, 40, 40],
   });
 
   assert({
-    given: "baseArr with identical items",
-    should: "return crossed sorted array",
+    given: 'baseArr with identical items',
+    should: 'return crossed sorted array',
     actual: cross([40, 40, 40, 40], [40]),
     expected: [40, 40, 40, 40],
   });
 
   assert({
-    given: "baseArr with identical items",
-    should: "return crossed sorted array",
+    given: 'baseArr with identical items',
+    should: 'return crossed sorted array',
     actual: cross([40, 40, 40, 40], [60]),
     expected: [40, 40, 40, 60],
   });
