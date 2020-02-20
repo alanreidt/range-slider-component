13708f64b1e030639ad93e419f9482cfaa90621a
Alan Reid <alanreidt@gmail.com>
2020-02-20 18:37:26 +0300

    Update findAntecedent function test Replace double quotes with single one

--- src/utilities/findAntecedent/findAntecedent.test.js
+++ src/utilities/findAntecedent/findAntecedent.test.js
@@ -1,14 +1,14 @@
-import { findAntecedent } from "./findAntecedent";
+import { findAntecedent } from './findAntecedent';
 import {
   makeTestClass,
   testDescriptionTemplate,
-} from "../../../tests/testUtilities";
+} from '../../../tests/testUtilities';
 
-describe("findAntecedent", () => {
-  const commonDescriptionTemplate = testDescriptionTemplate`antecedent of ${0} by ${1}, with offset = ${3} is ${"expectation"}`;
+describe('findAntecedent', () => {
+  const commonDescriptionTemplate = testDescriptionTemplate`antecedent of ${0} by ${1}, with offset = ${3} is ${'expectation'}`;
   const TestClass = makeTestClass(findAntecedent, commonDescriptionTemplate);
 
-  describe("shall return antecedent", () => {
+  describe('shall return antecedent', () => {
     const funcArgsList = [
       [500, 0.2],
       [500, 0],
@@ -24,7 +24,7 @@ describe("findAntecedent", () => {
     test.run(funcArgsList, expectations);
   });
 
-  describe("shall handle exceptions", () => {
+  describe('shall handle exceptions', () => {
     const funcArgsList = [
       [500, -0.2, 200],
       [500, 1.4, 200],
@@ -41,14 +41,14 @@ describe("findAntecedent", () => {
     test.run(funcArgsList, expectations);
   });
 
-  context("shall catch garbage input", () => {
-    describe("returns NaN, if consequent parameter is incorrect", () => {
+  context('shall catch garbage input', () => {
+    describe('returns NaN, if consequent parameter is incorrect', () => {
       const funcArgsList = [
         [undefined, 1, -500],
         [null, 1, -500],
         [NaN, 1, -500],
-        ["text", 1, -500],
-        ["123text", 1, -500],
+        ['text', 1, -500],
+        ['123text', 1, -500],
       ];
       const expectations = new Array(funcArgsList.length).fill(NaN);
       const test = new TestClass();
@@ -56,13 +56,13 @@ describe("findAntecedent", () => {
       test.run(funcArgsList, expectations);
     });
 
-    describe("returns NaN, if ratio parameter is incorrect", () => {
+    describe('returns NaN, if ratio parameter is incorrect', () => {
       const funcArgsList = [
         [100, undefined, -500],
         [100, null, -500],
         [100, NaN, -500],
-        [100, "text", -500],
-        [100, "123text", -500],
+        [100, 'text', -500],
+        [100, '123text', -500],
       ];
       const expectations = new Array(funcArgsList.length).fill(NaN);
       const test = new TestClass();
@@ -70,13 +70,13 @@ describe("findAntecedent", () => {
       test.run(funcArgsList, expectations);
     });
 
-    describe("returns NaN, if offset parameter is incorrect", () => {
+    describe('returns NaN, if offset parameter is incorrect', () => {
       const funcArgsList = [
         [100, 200, null],
         [100, 200, NaN],
         [100, 200, Infinity],
-        [100, 200, "text"],
-        [100, 200, "123text"],
+        [100, 200, 'text'],
+        [100, 200, '123text'],
       ];
       const expectations = new Array(funcArgsList.length).fill(NaN);
       const test = new TestClass();
