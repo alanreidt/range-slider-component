9dcbc3ec400e1d53b56e05e5eb74a8c7bf010557
Alan Reid <alanreidt@gmail.com>
2020-02-20 19:01:44 +0300

    Update isDivisible function Refactor by airbnb styleguide

--- src/utilities/isDivisible/isDivisible.js
+++ src/utilities/isDivisible/isDivisible.js
@@ -4,10 +4,12 @@
  * @param {number} dividend A number, that to be divided.
  * @param {number} divisor A number, that divides the dividend.
  */
-export function isDivisible(dividend, divisor) {
+const isDivisible = function isDivisibleFromUtilities(dividend, divisor) {
   if (dividend === null) {
     return false;
   }
 
   return dividend % divisor === 0;
-}
+};
+
+export default isDivisible;
