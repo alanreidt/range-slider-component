6b1e109bf5bf283a5d071be060b041335420bcf8
Alan Reid <alanreidt@gmail.com>
2020-02-20 20:21:04 +0300

    Update toPercentage function test Replace double quotes with single one

--- src/utilities/toPercentage/toPercentage.test.js
+++ src/utilities/toPercentage/toPercentage.test.js
@@ -1,30 +1,30 @@
-import { toPercentage } from "./toPercentage";
+import { toPercentage } from './toPercentage';
 import {
   makeTestClass,
   testDescriptionTemplate,
-} from "../../../tests/testUtilities";
+} from '../../../tests/testUtilities';
 
-describe("toPercentage", () => {
-  const commonDescriptionTemplate = testDescriptionTemplate`converts ${0} to ${"expectation"}`;
+describe('toPercentage', () => {
+  const commonDescriptionTemplate = testDescriptionTemplate`converts ${0} to ${'expectation'}`;
   const TestClass = makeTestClass(toPercentage, commonDescriptionTemplate);
 
-  describe("shall convert number to percentages", () => {
+  describe('shall convert number to percentages', () => {
     const funcArgsList = [[0], [2], [0.2], [0.75], [-2.5]];
-    const expectations = ["0%", "200%", "20%", "75%", "-250%"];
+    const expectations = ['0%', '200%', '20%', '75%', '-250%'];
     const test = new TestClass();
 
     test.run(funcArgsList, expectations);
   });
 
-  context("shall catch garbage input", () => {
-    describe("returns NaN, if number parameter is incorrect", () => {
+  context('shall catch garbage input', () => {
+    describe('returns NaN, if number parameter is incorrect', () => {
       const funcArgsList = [
         [undefined],
         [null],
         [Infinity],
         [NaN],
-        ["text"],
-        ["123text"],
+        ['text'],
+        ['123text'],
       ];
       const expectations = new Array(funcArgsList.length).fill(NaN);
       const test = new TestClass();
