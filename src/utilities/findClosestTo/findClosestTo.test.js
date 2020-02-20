e0a96f70005b5cde05e883feeb1bdefa5b918ce9
Alan Reid <alanreidt@gmail.com>
2020-02-20 18:54:04 +0300

    Update findClosestTo function test Replace double quotes with single one

--- src/utilities/findClosestTo/findClosestTo.test.js
+++ src/utilities/findClosestTo/findClosestTo.test.js
@@ -1,14 +1,14 @@
-import { findClosestTo } from "./findClosestTo";
+import { findClosestTo } from './findClosestTo';
 import {
   makeTestClass,
   testDescriptionTemplate,
-} from "../../../tests/testUtilities";
+} from '../../../tests/testUtilities';
 
-describe("findClosestTo", () => {
-  const commonDescriptionTemplate = testDescriptionTemplate`closest number to ${0} from ${"...rest"} is ${"expectation"}`;
+describe('findClosestTo', () => {
+  const commonDescriptionTemplate = testDescriptionTemplate`closest number to ${0} from ${'...rest'} is ${'expectation'}`;
   const TestClass = makeTestClass(findClosestTo, commonDescriptionTemplate);
 
-  describe("shall return closest number to the number", () => {
+  describe('shall return closest number to the number', () => {
     const funcArgsList = [
       [100, 50, 90],
       [-100, -50, -90],
@@ -23,7 +23,7 @@ describe("findClosestTo", () => {
     test.run(funcArgsList, expectations);
   });
 
-  describe("shall return last closest number to the number, if controversial", () => {
+  describe('shall return last closest number to the number, if controversial', () => {
     const funcArgsList = [
       [100, 99, 105, 101, 5],
       [100, 101, 105, 99, 5],
@@ -34,7 +34,7 @@ describe("findClosestTo", () => {
     test.run(funcArgsList, expectations);
   });
 
-  describe("shall filter incorrect arguments", () => {
+  describe('shall filter incorrect arguments', () => {
     const funcArgsList = [
       [100, null, 105, undefined, 5],
       [0, null, 105, Infinity, 5],
@@ -45,15 +45,15 @@ describe("findClosestTo", () => {
     test.run(funcArgsList, expectations);
   });
 
-  context("shall catch garbage input", () => {
-    describe("returns NaN if number parameter is incorrect", () => {
+  context('shall catch garbage input', () => {
+    describe('returns NaN if number parameter is incorrect', () => {
       const funcArgsList = [
         [undefined, -100, 100],
         [null, -100, 100],
         [Infinity, -100, 100],
         [NaN, -100, 100],
-        ["text", -100, 100],
-        ["123text", -100, 100],
+        ['text', -100, 100],
+        ['123text', -100, 100],
       ];
       const expectations = new Array(funcArgsList.length).fill(NaN);
       const test = new TestClass();
@@ -61,14 +61,14 @@ describe("findClosestTo", () => {
       test.run(funcArgsList, expectations);
     });
 
-    describe("returns NaN if variants parameter is incorrect", () => {
+    describe('returns NaN if variants parameter is incorrect', () => {
       const funcArgsList = [
         [50, undefined],
         [50, null],
         [50, Infinity],
         [50, NaN],
-        [50, "text"],
-        [50, "123text"],
+        [50, 'text'],
+        [50, '123text'],
       ];
       const expectations = new Array(funcArgsList.length).fill(NaN);
       const test = new TestClass();
