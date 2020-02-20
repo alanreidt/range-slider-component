4e60ee65193497b8420de3a990e389813f5d4daf
Alan Reid <alanreidt@gmail.com>
2020-02-20 14:35:37 +0300

    Update utilities index.js Replace "export from" with import and export pair

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,21 +1,44 @@
 /* PLOP_INJECT_EXPORT */
-export { cross } from './cross';
-export { replaceValueAt } from './replaceValueAt';
-export { isNumberFalsey } from './isNumberFalsey';
-export { restrictNumberByNeighbors } from './restrictNumberByNeighbors';
-export { either } from './either';
-export { isDivisible } from './isDivisible';
-export { findClosestFactor } from './findClosestFactor';
-export { findClosestDivisible } from './findClosestDivisible';
-export { isNumberInBetween } from './isNumberInBetween';
-export { isNumberBetween } from './isNumberBetween';
-export { findClosestTo } from './findClosestTo';
-export { findAntecedent } from './findAntecedent';
-export { toFixed } from './toFixed';
-export { toPercentage } from './toPercentage';
-export { placeNumberBetween } from './placeNumberBetween/placeNumberBetween';
-export { getAverageOf } from './getAverageOf/getAverageOf';
-export { getNextDivisibleOf } from './getNextDivisibleOf/getNextDivisibleOf';
-export { getPrevDivisibleOf } from './getPrevDivisibleOf/getPrevDivisibleOf';
-export { observerMixin } from './observerMixin/observerMixin';
-export { findRatio } from './findRatio';
+import { cross } from './cross';
+import { replaceValueAt } from './replaceValueAt';
+import { isNumberFalsey } from './isNumberFalsey';
+import { restrictNumberByNeighbors } from './restrictNumberByNeighbors';
+import { either } from './either';
+import { isDivisible } from './isDivisible';
+import { findClosestFactor } from './findClosestFactor';
+import { findClosestDivisible } from './findClosestDivisible';
+import { isNumberInBetween } from './isNumberInBetween';
+import { isNumberBetween } from './isNumberBetween';
+import { findClosestTo } from './findClosestTo';
+import { findAntecedent } from './findAntecedent';
+import { toFixed } from './toFixed';
+import { toPercentage } from './toPercentage';
+import { placeNumberBetween } from './placeNumberBetween/placeNumberBetween';
+import { getAverageOf } from './getAverageOf/getAverageOf';
+import { getNextDivisibleOf } from './getNextDivisibleOf/getNextDivisibleOf';
+import { getPrevDivisibleOf } from './getPrevDivisibleOf/getPrevDivisibleOf';
+import { observerMixin } from './observerMixin/observerMixin';
+import { findRatio } from './findRatio';
+
+export {
+  cross,
+  replaceValueAt,
+  isNumberFalsey,
+  restrictNumberByNeighbors,
+  either,
+  isDivisible,
+  findClosestFactor,
+  findClosestDivisible,
+  isNumberInBetween,
+  isNumberBetween,
+  findClosestTo,
+  findAntecedent,
+  toFixed,
+  toPercentage,
+  placeNumberBetween,
+  getAverageOf,
+  getNextDivisibleOf,
+  getPrevDivisibleOf,
+  observerMixin,
+  findRatio,
+};
