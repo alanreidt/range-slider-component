a32f2688926559eaa0754b5d060bb4cc0322b43f
Alan Reid <alanreidt@gmail.com>
2020-02-21 19:57:18 +0300

    Update test-page app.js Fix Slider imports paths

--- test-page/app.js
+++ test-page/app.js
@@ -1,24 +1,24 @@
-import { slider } from "../src/slider";
+import Slider from '../src/Slider';
 
-const parents = document.querySelectorAll(".js-slider-wrapper");
+const parents = document.querySelectorAll('.js-slider-wrapper');
 const parent0 = parents[0];
 const parent1 = parents[1];
 const parent2 = parents[2];
 
-slider.create(parent0, {
+Slider.create(parent0, {
   boundaries: [-30000, 30000],
   values: [-20000, 10000, 20000],
   step: 1000,
   hasTooltips: true,
 });
 
-slider.create(parent1, {
+Slider.create(parent1, {
   boundaries: [0, 233],
   step: 1,
   hasTooltips: true,
 });
 
-slider.create(parent2, {
-  orientation: "vertical",
+Slider.create(parent2, {
+  orientation: 'vertical',
   hasTooltips: true,
 });
