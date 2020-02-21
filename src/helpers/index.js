fdc52ff18cc398b081a1fe95f3b5b6dd00c299e4
Alan Reid <alanreidt@gmail.com>
2020-02-21 12:56:02 +0300

    Update placeBetween function Refactor by airbnb styleguide

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,7 +3,7 @@ import { replaceAt } from './replaceAt';
 import { restrictByNeighbors } from './restrictByNeighbors';
 import crossWith from './crossWith';
 import eitherOr from './eitherOr';
-import { placeBetween } from './placeBetween';
+import placeBetween from './placeBetween';
 import adjustToRange from './adjustToRange';
 import createTemplate from './createTemplate';
 import { findValueByRatioBetween } from './findValueByRatioBetween';
