c278010e56d21dafa8bb53c85a9e0e825b718622
Alan Reid <alanreidt@gmail.com>
2020-02-22 12:45:11 +0300

    Update composeClassTest test utility function Refactor according to airbnb styleguide

--- tests/testUtilities/composeClassTest.js
+++ tests/testUtilities/composeClassTest.js
@@ -1,26 +1,30 @@
-import { assert } from "chai";
+import { assert } from 'chai';
 
-export function composeClassTest({ Class, methodName, methodGetterName } = {}) {
+const composeClassTest = function composeClassTestFromTestUtilities({
+  Class,
+  methodName,
+  methodGetterName,
+} = {}) {
   return ({ constructorArgs, methodArgsList, expectations } = {}) => {
-    let constructorArgsRecord = "";
+    let constructorArgsRecord = '';
 
-    for (const constructorArgsKey in constructorArgs) {
+    Object.keys(constructorArgs).forEach((constructorArgsKey) => {
       const constructorArgsValue = constructorArgs[constructorArgsKey];
 
       constructorArgsRecord += `${constructorArgsKey} = ${constructorArgsValue}, `;
-    }
+    });
 
     expectations.forEach((expectation, index) => {
       const subject = new Class(constructorArgs);
       const methodArgs = methodArgsList && methodArgsList[index];
-      let methodArgsRecord = "";
+      let methodArgsRecord = '';
 
       if (methodName && methodArgs) {
-        for (const methodArgsKey in methodArgs) {
+        Object.keys(methodArgs).forEach((methodArgsKey) => {
           const methodArgsValue = methodArgs[methodArgsKey];
 
           methodArgsRecord += `${methodArgsKey} = ${methodArgsValue}, `;
-        }
+        });
 
         subject[methodName](methodArgs);
       }
@@ -32,18 +36,20 @@ export function composeClassTest({ Class, methodName, methodGetterName } = {}) {
       }
 
       const subjectOutput = subject[methodGetterName]();
-      methodArgsRecord = methodArgsRecord || "default values";
+      methodArgsRecord = methodArgsRecord || 'default values';
 
       context(`if ${methodArgsRecord} were passed`, () => {
-        for (const expectationKey in expectation) {
+        Object.keys(expectation).forEach((expectationKey) => {
           const expectationValue = expectation[expectationKey];
           const subjectOutputValue = subjectOutput[expectationKey];
 
-          it(`than ${expectationKey} shall be equal to ${expectationValue}`, function() {
+          it(`than ${expectationKey} shall be equal to ${expectationValue}`, () => {
             assert.deepEqual(subjectOutputValue, expectationValue);
           });
-        }
+        });
       });
     });
   };
-}
+};
+
+export default composeClassTest;
