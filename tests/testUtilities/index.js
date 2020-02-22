e4d8ab68f9a4a266e2c61672579fbd8a7f87b150
Alan Reid <alanreidt@gmail.com>
2020-02-22 13:20:19 +0300

    Update testUtilities index Fix simulateMouseEvent function import path

--- tests/testUtilities/index.js
+++ tests/testUtilities/index.js
@@ -1,7 +1,7 @@
 import composeClassTest from './composeClassTest';
 import makeTestClass from './makeTestClass';
 import { testDescriptionTemplate } from './testDescriptionTemplate';
-import { simulateMouseEvent } from './simulateMouseEvent';
+import simulateMouseEvent from './simulateMouseEvent';
 
 export {
   composeClassTest,
