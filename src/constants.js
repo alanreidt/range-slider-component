9b849efb0cf3e6dd4556782a052a5c75bc84cb3f
Alan Reid <alanreidt@gmail.com>
2020-02-23 12:47:19 +0300

    Update constants file Add constants for slider's elements classes

--- src/constants.js
+++ src/constants.js
@@ -17,21 +17,58 @@ const DEFAULT_OPTIONS = {
   },
 };
 
-const SLIDER_NAME = 'js-slider';
-const SLIDER_BASE_NAME = 'js-slider__base';
-const SLIDER_ORIENTATION_FLAG = 'js-slider_vertical';
-const SLIDER_HANDLE_GROUP_NAME = 'js-slider__handle-group';
-const SLIDER_TOOLTIP_NAME = 'js-slider__tooltip';
-const SLIDER_HANDLE_NAME = 'js-slider__handle';
+const SLIDER_NAME = 'slider';
+const ORIENTATION_FLAG_NAME = 'vertical';
+const BASE_NAME = 'base';
+const HANDLE_GROUP_NAME = 'handle-group';
+const TOOLTIP_NAME = 'tooltip';
+const HANDLE_NAME = 'handle';
+
+const SLIDER_VERTICAL_CLASS_NAME = 'slider_vertical';
+const BASE_CLASS_NAME = 'slider__base';
+const HANDLE_GROUP_CLASS_NAME = 'slider__handle-group';
+const TOOLTIP_CLASS_NAME = 'slider__tooltip';
+const HANDLE_CLASS_NAME = 'slider__handle';
+
+const JS_SLIDER_CLASS_NAME = 'js-slider';
+const JS_SLIDER_VERTICAL_CLASS_NAME = 'js-slider_vertical';
+const JS_BASE_CLASS_NAME = 'js-slider__base';
+const JS_HANDLE_GROUP_CLASS_NAME = 'js-slider__handle-group';
+const JS_TOOLTIP_CLASS_NAME = 'js-slider__tooltip';
+const JS_HANDLE_CLASS_NAME = 'js-slider__handle';
+
+const JS_SLIDER_SELECTOR = '.js-slider';
+const JS_SLIDER_VERTICAL_SELECTOR = '.js-slider_vertical';
+const JS_BASE_SELECTOR = '.js-slider__base';
+const JS_HANDLE_GROUP_SELECTOR = '.js-slider__handle-group';
+const JS_TOOLTIP_SELECTOR = '.js-slider__tooltip';
+const JS_HANDLE_SELECTOR = '.js-slider__handle';
 
 export {
   DEFAULT_OPTIONS,
   ORIENTATION_HORIZONTAL,
   ORIENTATION_VERTICAL,
   SLIDER_NAME,
-  SLIDER_BASE_NAME,
-  SLIDER_ORIENTATION_FLAG,
-  SLIDER_HANDLE_GROUP_NAME,
-  SLIDER_TOOLTIP_NAME,
-  SLIDER_HANDLE_NAME,
+  ORIENTATION_FLAG_NAME,
+  BASE_NAME,
+  HANDLE_GROUP_NAME,
+  TOOLTIP_NAME,
+  HANDLE_NAME,
+  SLIDER_VERTICAL_CLASS_NAME,
+  BASE_CLASS_NAME,
+  HANDLE_GROUP_CLASS_NAME,
+  TOOLTIP_CLASS_NAME,
+  HANDLE_CLASS_NAME,
+  JS_SLIDER_CLASS_NAME,
+  JS_SLIDER_VERTICAL_CLASS_NAME,
+  JS_BASE_CLASS_NAME,
+  JS_HANDLE_GROUP_CLASS_NAME,
+  JS_TOOLTIP_CLASS_NAME,
+  JS_HANDLE_CLASS_NAME,
+  JS_SLIDER_SELECTOR,
+  JS_SLIDER_VERTICAL_SELECTOR,
+  JS_BASE_SELECTOR,
+  JS_HANDLE_GROUP_SELECTOR,
+  JS_TOOLTIP_SELECTOR,
+  JS_HANDLE_SELECTOR,
 };
