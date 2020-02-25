deec8edbaf5f30897d9cc6cd782cd176fcd2f01d
Alan Reid <alanreidt@gmail.com>
2020-02-25 14:38:24 +0300

    Update variables position in app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,22 @@ import Slider from "../modules/slider/prod/Slider.min.js";
 
 const $parent = document.querySelector(".js-slider-wrapper");
 
-const $form = document.querySelector(".js-control-pane__form");
-const $boundariesTextInput = document.querySelector("#boundaries");
-const $valuesTextInput = document.querySelector("#values");
-const $stepNumberInput = document.querySelector("#step");
-const $orientationCheckbox = document.querySelector("#orientation");
-const $hasTooltipsCheckbox = document.querySelector("#hasTooltips");
-
 Slider.create($parent, {
   boundaries: [0, 100],
   values: [20, 80],
   hasTooltips: true,
 });
 
+const $form = document.querySelector(".js-control-pane__form");
+
 $form.addEventListener("submit", event => {
   event.preventDefault();
+
+  const $boundariesTextInput = document.querySelector("#boundaries");
+  const $valuesTextInput = document.querySelector("#values");
+  const $stepNumberInput = document.querySelector("#step");
+  const $orientationCheckbox = document.querySelector("#orientation");
+  const $hasTooltipsCheckbox = document.querySelector("#hasTooltips");
 
   const boundaries = $boundariesTextInput.value.split(", ");
   const values = $valuesTextInput.value.split(", ");
