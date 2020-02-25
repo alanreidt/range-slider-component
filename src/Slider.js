4c8543bef252f0cd5aed1473a053fc5027fa6c79
Alan Reid <alanreidt@gmail.com>
2020-02-25 15:53:32 +0300

    Update Slider.js methods description

--- src/Slider.js
+++ src/Slider.js
@@ -1,18 +1,18 @@
 import SliderFactory from './SliderFactory';
 
 /**
- * This object represents API for the Slider.
- * All interactions with the Slider must happen only through it.
+ * This object represents API for Slider.
+ * All interactions with Slider must happen only through it.
  */
 const Slider = {
   _factory: SliderFactory,
   _parentsMap: new WeakMap(),
 
   /**
-   * Create the Slider instance.
+   * Create Slider instance.
    *
-   * @param {HTMLElement} parent An element the Slider to be inserted in.
-   * @param {Object} options Options of the Slider.
+   * @param {HTMLElement} parent An element Slider to be inserted in.
+   * @param {Object} options Options of Slider.
    */
   create(parent, options) {
     const model = this._factory.createModel(options);
@@ -30,30 +30,30 @@ const Slider = {
   },
 
   /**
-   * Returns the Slider instance's current options. Non-primitive values are references.
+   * Returns Slider instance's current options copy. Non-primitive values are references.
    *
-   * @param {HTMLElement} parent An element the Slider is inserted in.
+   * @param {HTMLElement} parent An element Slider is inserted in.
    *
-   * @returns {Object} Current options of the Slider.
+   * @returns {Object} Current options of Slider.
    */
   getOptions(parent) {
     return this._parentsMap.get(parent).model.getOptions();
   },
 
   /**
-   * Set the Slider instance's options.
+   * Set Slider instance's options.
    *
-   * @param {HTMLElement} parent An element the Slider is inserted in.
-   * @param {Object} options Options to be set to the Slider.
+   * @param {HTMLElement} parent An element Slider is inserted in.
+   * @param {Object} options Options to be set to Slider.
    */
   setOptions(parent, options) {
     this._parentsMap.get(parent).model.setOptions(options);
   },
 
   /**
-   * Set the Slider instance's value of “values” option at index position.
+   * Set Slider instance's value of “values” option at index position.
    *
-   * @param {HTMLElement} parent An element the Slider is inserted in.
+   * @param {HTMLElement} parent An element Slider is inserted in.
    * @param {number} index An index of “values” option's value to change.
    * @param {number} value A value to set.
    */
