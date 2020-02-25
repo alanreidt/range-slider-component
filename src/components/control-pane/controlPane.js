4e84657704bf8214c0fd5ef4fed6731e21823cd8
Alan Reid <alanreidt@gmail.com>
2020-02-26 00:25:28 +0300

    Update controlPane.js Convert logic into class

--- src/components/control-pane/controlPane.js
+++ src/components/control-pane/controlPane.js
@@ -1,27 +1,43 @@
-const form = document.querySelector(".js-control-pane__form");
+class controlPane {
+  constructor(anchorElement, model) {
+    this.anchorElement = anchorElement;
+    this.model = model;
 
-form.addEventListener("submit", event => {
-  event.preventDefault();
+    this._bindMethods();
+    this._attachElements();
+    this.form.addEventListener('submit', this._handleFormSubmit);
+  }
 
-  const boundariesTextInput = document.querySelector(".js-boundaries-text-input");
-  const valuesTextInput = document.querySelector(".js-values-text-input");
-  const stepNumberInput = document.querySelector(".js-step-number-input");
-  const orientationCheckbox = document.querySelector(".js-orientation-checkbox");
-  const hasTooltipsCheckbox = document.querySelector(".js-hasTooltips-checkbox");
+  _bindMethods() {
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+  }
 
-  const boundaries = boundariesTextInput.value.split(", ");
-  const values = valuesTextInput.value.split(", ");
-  const step = stepNumberInput.valueAsNumber;
-  const orientation = orientationCheckbox.checked ? "vertical" : "horizontal";
-  const hasTooltips = hasTooltipsCheckbox.checked;
+  _attachElements() {
+    this.form = this.anchorElement.querySelector('.js-control-pane__form');
+    this.boundariesTextInput = this.anchorElement.querySelector('.js-boundaries-text-input');
+    this.valuesTextInput = this.anchorElement.querySelector('.js-values-text-input');
+    this.stepNumberInput = this.anchorElement.querySelector('.js-step-number-input');
+    this.orientationCheckbox = this.anchorElement.querySelector('.js-orientation-checkbox');
+    this.hasTooltipsCheckbox = this.anchorElement.querySelector('.js-hasTooltips-checkbox');
+  }
 
-  const options = {
-    boundaries,
-    values,
-    step,
-    hasTooltips,
-    orientation,
-  };
+  _handleFormSubmit(event) {
+    event.preventDefault();
 
-  Slider.create(parent, options);
-});
+    const boundaries = this.boundariesTextInput.value.split(', ');
+    const values = this.valuesTextInput.value.split(', ');
+    const step = this.stepNumberInput.valueAsNumber;
+    const orientation = this.orientationCheckbox.checked ? 'vertical' : 'horizontal';
+    const hasTooltips = this.hasTooltipsCheckbox.checked;
+
+    model.setOptions({
+      boundaries,
+      values,
+      step,
+      hasTooltips,
+      orientation,
+    });
+  }
+}
+
+export default controlPane;
