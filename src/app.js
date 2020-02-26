d93ecc2c9eb6ba3dfb014a763d75ccddf7c30282
Alan Reid <alanreidt@gmail.com>
2020-02-26 12:16:22 +0300

    Connect control pane to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import "bootstrap";
+
 import Slider from "../modules/slider/prod/Slider.min.js";
+import ControlPane from "./components/control-pane/ControlPane";
 
 const parent = document.querySelector(".js-slider-wrapper");
 
@@ -7,6 +9,14 @@ Slider.create(parent, {
   boundaries: [0, 100],
   values: [20, 80],
   hasTooltips: true,
+});
+
+const controlPaneAnchorElement = document.querySelector(".js-control-pane");
+
+ControlPane.create(controlPaneAnchorElement, {});
+
+ControlPane.addSubscriber(controlPaneAnchorElement, "update", (options) => {
+  Slider.create(parent, options);
 });
 
 const currentValuesTextInput = document.querySelector(".js-current-values-text-input");
