09f26cb966ce146da7f87a1f26e965a004fedac2
Alan Reid <alanreidt@gmail.com>
2020-02-26 12:50:28 +0300

    Add control input ViewController logic

---
+++ src/components/control-input/ViewController.js
@@ -0,0 +1,36 @@
+class ViewController {
+  constructor(anchorElement, model) {
+    this.anchorElement = anchorElement;
+    this.model = model;
+
+    this._bindMethods();
+    this._attachElements();
+    this.form.addEventListener('submit', this._handleFormSubmit);
+    this.setElements(model.getOptions());
+  }
+
+  setElements({ values } = {}) {
+    this.currentValuesTextInput.setAttributes('value', values.join(', '));
+  }
+
+  _bindMethods() {
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+  }
+
+  _attachElements() {
+    this.form = this.anchorElement.querySelector('.js-slider-pane__form');
+    this.currentValuesTextInput = this.anchorElement.querySelector('.js-current-values-text-input');
+  }
+
+  _handleFormSubmit(event) {
+    event.preventDefault();
+
+    const values = this.currentValuesTextInput.value.split(', ');
+
+    this.model.setOptions({
+      values,
+    });
+  }
+}
+
+export default ViewController;
