185fa34f815b50d4cc3bde8c7a6bdfc54dda63b9
Alan Reid <alanreidt@gmail.com>
2020-03-01 17:11:24 +0300

    Create data object

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const optionsSets = [
+  {
+    title: 'default example',
+    options: {},
+  },
+  {
+    title: 'custom example',
+    options: {
+      boundaries: [-200, 300],
+      values: [-100, 200],
+      step: 10,
+      orientation: 'horizontal',
+      hasTooltips: true,
+    },
+  },
+  {
+    title: 'vertical example',
+    options: {
+      boundaries: [-500, 500],
+      values: [-300, -200, 200],
+      step: 25,
+      orientation: 'vertical',
+      hasTooltips: true,
+    },
+  }
+];
+
+export { optionsSets };
