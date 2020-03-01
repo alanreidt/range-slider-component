f0d6b2ea3a469a7cc883b7b2b670f09938a13696
Alan Reid <alanreidt@gmail.com>
2020-03-01 17:32:26 +0300

    Update App.js Pass dataSet object through App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import 'bootstrap';
 
+import { dataSet } from './data'
 import PlayGround from './components/playground/PlayGround';
 
 class App {
-  constructor(anchorElement, options) {
+  constructor(anchorElement, dataSet) {
     this.anchorElement = anchorElement;
 
     this._attachElements();
-    this._createComponents(options);
+    this._createComponents(dataSet);
   }
 
   _attachElements() {
     this.playgrounds = this.anchorElement.querySelectorAll('.js-playground');
   }
 
-  _createComponents(options) {
-    this.playgrounds.forEach((playground) => {
-      new PlayGround(playground, options);
+  _createComponents(dataSet) {
+    this.playgrounds.forEach((playground, index) => {
+      const data = dataSet[index];
+
+      new PlayGround(playground, data.options);
     });
   }
 }
 
 const appElement = document.querySelector('.js-app');
 
-window.addEventListener("load", new App(appElement, {
-  boundaries: [-200, 300],
-  values: [-100, 200],
-  step: 20,
-  orientation: 'horizontal',
-  hasTooltips: true,
-}));
+window.addEventListener("load", new App(appElement, dataSet));
