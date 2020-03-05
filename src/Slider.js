258086818243e64241d4254eee23b98f038beb60
Alan Reid <alanreidt@gmail.com>
2020-03-05 10:27:07 +0300

    Realize Slider API in form of a Class

--- src/Slider.js
+++ src/Slider.js
@@ -4,9 +4,21 @@ import SliderFactory from './SliderFactory';
  * This object represents API for Slider.
  * All interactions with Slider must happen only through it.
  */
-const Slider = {
-  _factory: SliderFactory,
-  _parentsMap: new WeakMap(),
+class Slider {
+  constructor(parent, options, Factory = SliderFactory) {
+    this._factory = Factory;
+    this.model = this._factory.createModel(options);
+    this.viewController = this._factory.createViewController(
+      parent,
+      this.model,
+    );
+
+    const viewControllerSetElementsBound = this.viewController.setElements.bind(
+      this.viewController,
+    );
+
+    this.model.addSubscriber('update', viewControllerSetElementsBound);
+  }
 
   /**
    * Create Slider instance.
@@ -14,20 +26,13 @@ const Slider = {
    * @param {HTMLElement} parent An element Slider to be inserted in.
    * @param {Object} options Options of Slider.
    */
-  create(parent, options) {
-    const model = this._factory.createModel(options);
-    const viewController = this._factory.createViewController(parent, model);
-    const viewControllerSetElementsBound = viewController.setElements.bind(
-      viewController,
-    );
+  static create(parent, options) {
+    const slider = new this(parent, options);
 
-    model.addSubscriber('update', viewControllerSetElementsBound);
+    this._parentsMap.set(parent, slider);
 
-    this._parentsMap.set(parent, {
-      model,
-      viewController,
-    });
-  },
+    return slider;
+  }
 
   /**
    * Returns Slider instance's current options copy. Non-primitive values are references.
@@ -36,9 +41,13 @@ const Slider = {
    *
    * @returns {Object} Current options of Slider.
    */
-  getOptions(parent) {
-    return this._parentsMap.get(parent).model.getOptions();
-  },
+  static getOptions(parent) {
+    return this._parentsMap.get(parent).getOptions();
+  }
+
+  getOptions() {
+    return this.model.getOptions();
+  }
 
   /**
    * Set Slider instance's options.
@@ -46,9 +55,13 @@ const Slider = {
    * @param {HTMLElement} parent An element Slider is inserted in.
    * @param {Object} options Options to be set to Slider.
    */
-  setOptions(parent, options) {
-    this._parentsMap.get(parent).model.setOptions(options);
-  },
+  static setOptions(parent, options) {
+    this._parentsMap.get(parent).setOptions(options);
+  }
+
+  setOptions(options) {
+    this.model.setOptions(options);
+  }
 
   /**
    * Set Slider instance's value of “values” option at index position.
@@ -57,9 +70,13 @@ const Slider = {
    * @param {number} index An index of “values” option's value to change.
    * @param {number} value A value to set.
    */
-  setValueAt(parent, index, value) {
-    this._parentsMap.get(parent).model.setValueAt(index, value);
-  },
+  static setValueAt(parent, index, value) {
+    this._parentsMap.get(parent).setValueAt(index, value);
+  }
+
+  setValueAt(index, value) {
+    this.model.setValueAt(index, value);
+  }
 
   /**
    * Subscribe to event, usage:
@@ -69,9 +86,13 @@ const Slider = {
    * @param {string} eventName The name of an event to listen to.
    * @param {function} subscriber The subscriber to be triggered on the event.
    */
-  addSubscriber(parent, eventName, subscriber) {
-    this._parentsMap.get(parent).model.addSubscriber(eventName, subscriber);
-  },
+  static addSubscriber(parent, eventName, subscriber) {
+    this._parentsMap.get(parent).addSubscriber(eventName, subscriber);
+  }
+
+  addSubscriber(eventName, subscriber) {
+    this.model.addSubscriber(eventName, subscriber);
+  }
 
   /**
    * Cancel the subscription, usage:
@@ -80,9 +101,13 @@ const Slider = {
    * @param {string} eventName The name of an event to which subscriber is listen to.
    * @param {function} subscriber The subscriber to be removed from the list.
    */
-  removeSubscriber(parent, eventName, subscriber) {
-    this._parentsMap.get(parent).model.removeSubscriber(eventName, subscriber);
-  },
+  static removeSubscriber(parent, eventName, subscriber) {
+    this._parentsMap.get(parent).removeSubscriber(eventName, subscriber);
+  }
+
+  removeSubscriber(eventName, subscriber) {
+    this.model.removeSubscriber(eventName, subscriber);
+  }
 
   /**
    * Generate an event with the given name and data, usage:
@@ -91,9 +116,15 @@ const Slider = {
    * @param {string} eventName The name of an event to trigger.
    * @param {any} arg1...args The data to be passed to subscribers.
    */
-  triggerSubscribers(parent, eventName, ...args) {
-    this._parentsMap.get(parent).model.triggerSubscribers(eventName, ...args);
-  },
-};
+  static triggerSubscribers(parent, eventName, ...args) {
+    this._parentsMap.get(parent).triggerSubscribers(eventName, ...args);
+  }
+
+  triggerSubscribers(eventName, ...args) {
+    this.model.triggerSubscribers(eventName, ...args);
+  }
+}
+
+Slider._parentsMap = new WeakMap();
 
 export default Slider;
