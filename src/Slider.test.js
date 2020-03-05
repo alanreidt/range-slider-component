8529e072388dde9f52ce862446f33e7205708142
Alan Reid <alanreidt@gmail.com>
2020-03-05 19:15:11 +0300

    Update Slider test Add Slider instance methods test

--- src/Slider.test.js
+++ src/Slider.test.js
@@ -74,4 +74,76 @@ describe('Slider', () => {
       });
     });
   });
+
+  describe('Slider instance methods', () => {
+    describe('create method', () => {
+      it('shall create model with options', () => {
+        const options = {
+          boundaries: [100, 500],
+          values: [200, 300],
+          step: 20,
+          orientation: 'horizontal',
+          hasTooltips: true,
+        };
+        const parent = document.createElement('div');
+
+        const slider = Slider.create(parent, options);
+
+        assert.deepEqual(slider.getOptions(), options);
+      });
+    });
+
+    describe('setOptions method', () => {
+      it('shall set options', () => {
+        const options = {
+          boundaries: [100, 500],
+          values: [200, 300],
+          step: 20,
+          orientation: 'horizontal',
+          hasTooltips: true,
+        };
+        const parent = document.createElement('div');
+
+        const slider = Slider.create(parent, options);
+
+        const newOptions = {
+          boundaries: [-200, 1000],
+          step: 10,
+        };
+
+        slider.setOptions(newOptions);
+
+        const expectation = {
+          ...options,
+          ...newOptions,
+        };
+
+        assert.deepEqual(slider.getOptions(), expectation);
+      });
+    });
+
+    describe('setValueAt method', () => {
+      it('shall set value at index', () => {
+        const options = {
+          boundaries: [100, 500],
+          values: [200, 300],
+          step: 20,
+          orientation: 'horizontal',
+          hasTooltips: true,
+        };
+        const parent = document.createElement('div');
+
+        const slider = Slider.create(parent, options);
+
+        slider.setValueAt(1, 500);
+
+        const expectation = {
+          ...options,
+          values: [200, 500],
+        };
+
+        assert.deepEqual(slider.getOptions(), expectation);
+      });
+    });
+  });
 });
