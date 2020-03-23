d162a970e65e199f537c430dec236af69747f572
Alan Reid <alanreidt@gmail.com>
2020-03-23 12:21:43 +0300

    Update examples in app.js

--- test-page/app.js
+++ test-page/app.js
@@ -6,15 +6,15 @@ const parent1 = parents[1];
 const parent2 = parents[2];
 
 Slider.create(parent0, {
-  boundaries: [-30000, 30000],
-  values: [-20000, 10000, 20000],
-  step: 1000,
+  boundaries: [10, 50],
+  values: [10, 30, 50],
+  step: 1,
   hasTooltips: true,
 });
 
 Slider.create(parent1, {
-  boundaries: [0, 233],
-  step: 1,
+  boundaries: [0, 333],
+  step: 9,
   hasTooltips: true,
 });
 
