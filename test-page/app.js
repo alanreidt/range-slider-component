19f766aeeafab7a2115b9b2a0d6a97d45438dbf6
Alan Reid <alanreidt@gmail.com>
2020-03-24 10:48:17 +0300

    Update test-page examples

--- test-page/app.js
+++ test-page/app.js
@@ -4,13 +4,15 @@ const parents = document.querySelectorAll('.js-slider-wrapper');
 const parent0 = parents[0];
 const parent1 = parents[1];
 const parent2 = parents[2];
+const parent3 = parents[3];
 
 Slider.create(parent0, {
-  boundaries: [10, 50],
-  values: [10, 30, 50],
-  step: 1,
+  boundaries: [1000, 5000],
+  values: [1000, 3000, 5000],
+  step: 1000,
   hasTooltips: true,
   hasScale: true,
+  theme: 'modern',
 });
 
 Slider.create(parent1, {
@@ -20,6 +22,16 @@ Slider.create(parent1, {
 });
 
 Slider.create(parent2, {
+  values: [20, 80],
   orientation: 'vertical',
   hasTooltips: true,
+  hasScale: true,
+  theme: 'modern',
+});
+
+Slider.create(parent3, {
+  values: [20, 80],
+  orientation: 'vertical',
+  hasTooltips: true,
+  hasScale: true,
 });
