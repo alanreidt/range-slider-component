3cee3cf28e15e1dc894e3dd6109f4f49aa8ffa8e
Alan Reid <alanreidt@gmail.com>
2020-04-14 18:50:27 +0300

    Update ViewController Fix convertion of an event position to the Slider value

--- src/ViewController/ViewController.js
+++ src/ViewController/ViewController.js
@@ -178,30 +178,30 @@ class ViewController {
   }
 
   _convertCoordinateToValue({ xCoordinate, yCoordinate }) {
-    const [adjustedCoordinate, sliderSize] = !isUndefined(xCoordinate)
-      ? [this._adjustToSliderXAxis(xCoordinate), this._getSliderWidth()]
-      : [this._adjustToSliderYAxis(yCoordinate), this._getSliderHeight()];
+    const [adjustedCoordinate, baseSize] = !isUndefined(xCoordinate)
+      ? [this._adjustToBaseXAxis(xCoordinate), this._getBaseWidth()]
+      : [this._adjustToBaseYAxis(yCoordinate), this._getBaseHeight()];
 
-    const ratio = findRatio(adjustedCoordinate, sliderSize);
+    const ratio = findRatio(adjustedCoordinate, baseSize);
     const { boundaries } = this._model.getOptions();
 
     return findValueByRatioBetween(ratio, ...boundaries);
   }
 
-  _adjustToSliderXAxis(xCoordinate) {
-    return xCoordinate - this._slider.getBoundingClientRect().left;
+  _adjustToBaseXAxis(xCoordinate) {
+    return xCoordinate - this._base.getBoundingClientRect().left;
   }
 
-  _adjustToSliderYAxis(yCoordinate) {
-    return yCoordinate - this._slider.getBoundingClientRect().top;
+  _adjustToBaseYAxis(yCoordinate) {
+    return yCoordinate - this._base.getBoundingClientRect().top;
   }
 
-  _getSliderWidth() {
-    return this._slider.getBoundingClientRect().width;
+  _getBaseWidth() {
+    return this._base.getBoundingClientRect().width;
   }
 
-  _getSliderHeight() {
-    return this._slider.getBoundingClientRect().height;
+  _getBaseHeight() {
+    return this._base.getBoundingClientRect().height;
   }
 }
 
