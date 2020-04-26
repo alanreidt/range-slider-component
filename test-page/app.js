a2f62ed3b78c1f314dcf014c51f120add4bb709f
Alan Reid <alanreidt@gmail.com>
2020-04-26 12:57:28 +0300

    Update test-page app.js Automate the creation of the Slider components

--- test-page/app.js
+++ test-page/app.js
@@ -1,37 +1,38 @@
 import Slider from '../src/Slider';
 
-const parents = document.querySelectorAll('.js-slider-wrapper');
-const parent0 = parents[0];
-const parent1 = parents[1];
-const parent2 = parents[2];
-const parent3 = parents[3];
+const sliderWrappers = document.querySelectorAll('.js-slider-wrapper');
 
-Slider.create(parent0, {
-  boundaries: [1000, 5000],
-  values: [1000, 3000, 5000],
-  step: 1000,
-  hasTooltips: true,
-  hasScale: true,
-  theme: 'modern',
-});
+const options = [
+  {
+    boundaries: [1000, 5000],
+    values: [1000, 3000, 5000],
+    step: 1000,
+    hasTooltips: true,
+    hasScale: true,
+    theme: 'modern',
+  },
+  {
+    boundaries: [0, 333],
+    step: 9,
+    hasTooltips: true,
+  },
+  {
+    values: [20, 80],
+    isVertical: true,
+    hasTooltips: true,
+    hasScale: true,
+    theme: 'modern',
+  },
+  {
+    values: [20, 80],
+    isVertical: true,
+    hasTooltips: true,
+    hasScale: true,
+  },
+];
 
-Slider.create(parent1, {
-  boundaries: [0, 333],
-  step: 9,
-  hasTooltips: true,
-});
+options.forEach((option, index) => {
+  const sliderWrapper = sliderWrappers[index];
 
-Slider.create(parent2, {
-  values: [20, 80],
-  isVertical: true,
-  hasTooltips: true,
-  hasScale: true,
-  theme: 'modern',
-});
-
-Slider.create(parent3, {
-  values: [20, 80],
-  isVertical: true,
-  hasTooltips: true,
-  hasScale: true,
+  Slider.create(sliderWrapper, option);
 });
